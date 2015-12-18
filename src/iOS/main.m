fe3c6fac723d62fb57792a0a91169f357e11db59
Mikael Rekola <mikael.rekola@gmail.com>
2015-12-19 01:46:02 +0200

    add main.m

---
+++ src/iOS/main.m
@@ -0,0 +1,23 @@
+#import <Foundation/Foundation.h>
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+#if 1
+
+int main(int argc, char *argv[])
+{
+  NSAutoreleasePool * pool = [[NSAutoreleasePool alloc] init];
+  int retVal = UIApplicationMain(argc, argv, nil, @"AppDelegate");
+  [pool release];
+  return retVal;
+}
+
+#else
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
+
+#endif
