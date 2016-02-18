9293be60a3f7f86ae7f84f69022b1fda3a0a0092
Mikael Rekola <mikael.rekola@gmail.com>
2016-02-19 01:21:44 +0200

    add shader_program

---
+++ include/shader_program.h
@@ -0,0 +1,48 @@
+#ifndef _SHADER_PROGRAM_H_
+#define _SHADER_PROGRAM_H_
+
+#include <glm/glm.hpp>
+#include <string>
+#include <set>
+#include <unordered_map>
+
+namespace gpufw {
+  class shader_program {
+  public:
+    shader_program();
+    shader_program(const shader_program & other) = delete;
+    shader_program & operator=(const shader_program & other) = delete;
+
+    bool loadShaders(const std::string & glsl_version, const std::string & filename) { return loadShaders(glsl_version.c_str(), filename.c_str()); }
+    bool loadShaders(const char * glsl_version, const char * filename);
+    bool loadShaderFromFile(GLenum type, const char * glsl_version, const char * filename);
+    bool loadShader(GLenum type, const char * glsl_version, const char * shaderSrc, const char * filename = 0);
+    
+    bool link();
+    void use();
+    void unuse();
+    
+    void bindAttribLocation(unsigned int index, const char * name);
+    int getUniformLocation(const std::string & name);
+    bool isValid() const { return programObject != 0; }
+    
+    void setUniform(const std::string & name, const glm::mat4 & mat);
+    void setUniform(const std::string & name, int s);
+    void setUniform(const std::string & name, const glm::vec4 & v);
+    void setUniform(const std::string & name, const glm::vec3 & v);
+    void setUniform(const std::string & name, const glm::ivec2 & v);
+    void setUniform(const std::string & name, float f);
+    
+    void define(const char * s) { defines.insert(s); }
+    
+  private:
+    unsigned int programObject = 0;
+    unsigned int vertexShader = 0;
+    unsigned int fragmentShader = 0;
+    std::set<unsigned int> bound_attribs;
+    std::unordered_map<std::string, int> uniform_locations;
+    std::set<std::string> defines;
+  };
+};
+
+#endif
---
+++ src/shader_program.cpp
@@ -0,0 +1,220 @@
+#include "GL.h"
+
+#include "shader_program.h"
+
+#include <iostream>
+#include <fstream>
+#include <sstream>
+#include <cassert>
+
+#include <glm/gtc/type_ptr.hpp>
+
+using namespace std;
+using namespace gpufw;
+
+shader_program::shader_program() { }
+
+bool
+shader_program::loadShaders(const char * glsl_version, const char * filename) {
+  std::ifstream t(filename);
+  std::stringstream buffer;
+  buffer << t.rdbuf();
+  
+  bool r = true;
+  r = loadShader(GL_VERTEX_SHADER, glsl_version, buffer.str().c_str(), filename);
+  if (r) {
+    r = loadShader(GL_FRAGMENT_SHADER, glsl_version, buffer.str().c_str(), filename);
+  }
+  return r;
+}
+
+bool
+shader_program::loadShaderFromFile(GLenum type, const char * glsl_version, const char * filename) {
+  std::ifstream t(filename);
+  std::stringstream buffer;
+  buffer << t.rdbuf();
+  return loadShader(type, glsl_version, buffer.str().c_str(), filename);
+}
+
+bool
+shader_program::loadShader(GLenum type, const char * glsl_version, const char * shaderSrc, const char * filename) {
+  GLuint shader;
+  GLint compiled;
+
+  // Create the shader object
+  shader = glCreateShader( type );
+  assert(shader != 0);
+  if (shader == 0) {
+    return 0;
+  }
+
+  const char * strings[64]; // 4 + 2 * defines.size()];
+  unsigned int si = 0;
+  strings[si++] = glsl_version;
+  strings[si++] = "\n";
+  if (type == GL_VERTEX_SHADER) {
+    strings[si++] = "#define VERTEX\n";
+  } else if (type == GL_FRAGMENT_SHADER) {
+    strings[si++] = "#define FRAGMENT\n";
+  }
+  for (auto & d : defines) {
+    strings[si++] = d.c_str();
+    strings[si++] = "\n";
+  }
+  strings[si++] = "#line 1\n";
+  strings[si++] = shaderSrc;
+
+  // Load the shader source
+  glShaderSource( shader, 5 + 2 * defines.size(), strings, NULL);
+  
+  // Compile the shader
+  glCompileShader( shader );
+  
+  // Check the compile status
+  glGetShaderiv( shader, GL_COMPILE_STATUS, &compiled );
+  
+  if (!compiled) {
+    GLint infoLen = 0;
+    
+    glGetShaderiv ( shader, GL_INFO_LOG_LENGTH, &infoLen );
+
+    string infoLog;
+    if (infoLen > 1) {
+      char * tmp = new char[infoLen + 1];
+      glGetShaderInfoLog ( shader, infoLen, NULL, tmp );
+      tmp[infoLen] = 0;
+      infoLog = tmp;
+      delete[] tmp;
+    }
+     
+    string msg = "Error compiling ";
+    msg += (type == GL_VERTEX_SHADER ? "vertex" : "fragment" );
+    msg += " shader (";
+    if (filename) msg += filename;
+    msg += "):\n";
+    msg += infoLog;
+
+    cerr << msg << endl;
+      
+    glDeleteShader ( shader );
+    return false;
+  }
+
+  switch (type) {
+  case GL_VERTEX_SHADER:
+    vertexShader = shader;
+    break;
+  case GL_FRAGMENT_SHADER:
+    fragmentShader = shader;
+    break;
+  }
+
+  return true;
+}
+
+bool
+shader_program::link() {
+  if (!programObject) programObject = glCreateProgram();  
+  if (vertexShader) glAttachShader(programObject, vertexShader);
+  if (fragmentShader) glAttachShader(programObject, fragmentShader);
+
+  // Link the program
+  glLinkProgram( programObject );
+
+  // Check the link status
+  GLint linked;
+  glGetProgramiv( programObject, GL_LINK_STATUS, &linked );
+
+  if ( !linked ) {
+    GLint infoLen = 0;
+    
+    glGetProgramiv ( programObject, GL_INFO_LOG_LENGTH, &infoLen );
+    
+    string infoLog;
+    if ( infoLen > 1 ) {
+      char * tmp = new char[infoLen + 1];    
+      glGetProgramInfoLog( programObject, infoLen, NULL, tmp );
+      tmp[infoLen] = 0;
+      infoLog = tmp;      
+      delete[] tmp;
+    }
+
+    cerr << "Error linking program:\n" << infoLog << endl;
+    
+    glDeleteProgram ( programObject );
+    return false;
+  }
+
+  if (vertexShader) glDetachShader(programObject, vertexShader);
+  if (fragmentShader) glDetachShader(programObject, fragmentShader);
+
+  return true;
+}
+
+void
+shader_program::use() {
+  assert(programObject > 0);
+  glUseProgram(programObject);
+  for (auto i : bound_attribs) {
+    glEnableVertexAttribArray(i);
+  }
+}
+
+void
+shader_program::unuse() {
+  for (auto i : bound_attribs) {
+    glDisableVertexAttribArray(i);
+  }
+  glUseProgram(0);
+}
+
+void
+shader_program::bindAttribLocation(unsigned int index, const char * name) {
+  if (!programObject) programObject = glCreateProgram();  
+  glBindAttribLocation(programObject, index, name);
+  bound_attribs.insert(index);
+}
+
+int
+shader_program::getUniformLocation(const std::string & name) {
+  auto it = uniform_locations.find(name);
+  if (it != uniform_locations.end()) {
+    return it->second;
+  } else {
+    cerr << "getUniformLocation(" << name << "): n = " << uniform_locations.size() << endl;
+    if (!programObject) programObject = glCreateProgram();  
+    int loc = glGetUniformLocation(programObject, name.c_str());
+    uniform_locations[name] = loc;
+    return loc;
+  }
+}
+
+void
+shader_program::setUniform(const std::string & name, const glm::mat4 & mat) {
+  glUniformMatrix4fv(getUniformLocation(name), 1, GL_FALSE, glm::value_ptr(mat));
+}
+
+void
+shader_program::setUniform(const std::string & name, int s) {
+  glUniform1i(getUniformLocation(name), s);
+}
+
+void
+shader_program::setUniform(const std::string & name, const glm::vec4 & v) {
+  glUniform4fv(getUniformLocation(name), 1, glm::value_ptr(v));
+}
+
+void
+shader_program::setUniform(const std::string & name, const glm::vec3 & v) {
+  glUniform3fv(getUniformLocation(name), 1, glm::value_ptr(v));
+}
+
+void
+shader_program::setUniform(const std::string & name, const glm::ivec2 & v) {
+  glUniform2iv(getUniformLocation(name), 1, glm::value_ptr(v));
+}
+
+void
+shader_program::setUniform(const std::string & name, float f) {
+  glUniform1f(getUniformLocation(name), f);
+}
