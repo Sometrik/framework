ebc694999230d2b0b27c2d493c1ce16577ac5790
Mikael Rekola <mikael.rekola@gmail.com>
2016-02-19 01:37:43 +0200

    add VBO

---
+++ include/VBO.h
@@ -0,0 +1,114 @@
+#ifndef _VBO_H_
+#define _VBO_H_
+
+#include <cstddef>
+#include <vector>
+
+#include <glm/glm.hpp>
+
+#if 1
+#include <TextureRef.h>
+#endif
+
+struct vbo_data_s {
+  glm::vec2 tex;
+  glm::vec3 normal;
+  glm::vec3 position;
+};
+
+struct vbo_data2_s {
+  glm::vec2 tex;
+  glm::vec4 color;
+  glm::vec3 normal;
+  glm::vec3 position;
+};
+
+struct vbo_data3_s {
+  glm::vec4 color;
+  glm::vec3 normal;
+  glm::vec3 position;
+};
+
+struct billboard_data_s {
+  glm::vec3 center_pos;
+  glm::uint32 corner_pos;  
+  glm::uint32 tex;
+};
+
+struct line_data_s {
+  unsigned char r, g, b, a;
+  glm::vec3 position;
+  float node_age, node_radius;
+};
+
+struct arc_data_2d_s {
+  unsigned char r, g, b, a;
+  glm::vec2 position;
+};
+
+struct node_vbo_s {
+  unsigned char r, g, b, a; // 0
+  glm::uint32 normal; // 4
+  glm::vec3 position; // 8
+  float age, size; // 20
+  short texture, flags; // 28
+};
+
+class VBO {
+ public:
+  enum DataType { C4F_N3F_V3F, T2F_N3F_V3F, T2F_C4F_N3F_V3F, NODES, BILLBOARDS, EDGES, ARCS_2D };
+  enum DrawType { NONE = 0, POINTS, LINES, TRIANGLES, TRIANGLE_STRIP, TRIANGLE_FAN };
+
+  VBO() { }
+  virtual ~VBO();
+
+  bool isDefined() const { return vbo != 0; }
+  void upload(DataType type, const void * ptr, size_t size);
+  void uploadIndices(const void * ptr, size_t size);
+  
+  void setDrawType(DrawType type) { default_draw_type = type; }
+
+  void draw(DrawType type = NONE);
+  void draw(DrawType type, const std::vector<int> & indices);
+
+  void quad2d(float x1, float y1,
+	      float x2, float y2,
+	      float x3, float y3,
+	      float x4, float y4);
+  void quad2d(float x1, float y1, float tx1, float ty1,
+	      float x2, float y2, float tx2, float ty2,
+	      float x3, float y3, float tx3, float ty3,
+	      float x4, float y4, float tx4, float ty4);
+  void sphere(float radius, unsigned int u, unsigned int v);
+  void ring(float outer_radius, float inner_radus, unsigned int n, float dx = 0, float dy = 0);
+
+  unsigned int getNumIndices() const { return num_indices; }
+  unsigned int getNumElements() const { return num_elements; }
+
+#if 1
+  const canvas::TextureRef & getTexture() const { return texture; }
+  void setTexture(const canvas::TextureRef & _texture) { texture = _texture; } 
+  void clearTexture() { texture.clear(); }
+  bool hasTexture() const { return texture.get() != 0; }
+#endif
+
+ protected:
+  void bind();
+  void unbind();
+
+  DataType getDataType() const { return data_type; }
+  int getStride() const { return stride; }
+
+  unsigned int vao = 0, vbo = 0, indexVbo = 0;
+#if 1
+  canvas::TextureRef texture;
+#endif
+
+ private:
+  DrawType default_draw_type = NONE;
+  DataType data_type = C4F_N3F_V3F;
+  int stride = 0;
+  unsigned int num_indices = 0, num_elements = 0;  
+};
+
+#endif
---
+++ src/VBO.cpp
@@ -0,0 +1,271 @@
+#include "GL.h"
+#include "VBO.h"
+
+#include <cassert>
+#include <iostream>
+
+using namespace std;
+
+static GLenum getGLDrawType(VBO::DrawType type) {
+  switch (type) {
+  case VBO::NONE: break;
+  case VBO::POINTS: return GL_POINTS;
+  case VBO::LINES: return GL_LINES;
+  case VBO::TRIANGLES: return GL_TRIANGLES;
+  case VBO::TRIANGLE_STRIP: return GL_TRIANGLE_STRIP;
+  case VBO::TRIANGLE_FAN: return GL_TRIANGLE_FAN;
+  }
+  assert(0);
+  return 0;
+}
+
+VBO::~VBO() {
+  if (vbo) {
+    glDeleteBuffers(1, &vbo);
+  }
+  if (indexVbo) {
+    glDeleteBuffers(1, &indexVbo);
+  }
+}
+
+void
+VBO::bind() {
+  // assert(num_elements > 0);
+  if (getTexture().get()) {
+    glBindTexture(GL_TEXTURE_2D, texture.getTextureId());
+  }
+  if (vbo) {
+    glBindBuffer(GL_ARRAY_BUFFER, vbo);
+    switch (getDataType()) {
+    case C4F_N3F_V3F:
+      glVertexAttribPointer(0, 4, GL_FLOAT, GL_FALSE, getStride(), (void *)(0 * sizeof(float)));
+      glVertexAttribPointer(1, 3, GL_FLOAT, GL_FALSE, getStride(), (void *)(7 * sizeof(float)));
+      break;
+    case T2F_N3F_V3F:
+      glVertexAttribPointer(0, 2, GL_FLOAT, GL_FALSE, getStride(), (void *)(0 * sizeof(float)));
+      glVertexAttribPointer(1, 3, GL_FLOAT, GL_FALSE, getStride(), (void *)(5 * sizeof(float)));
+      break;
+    case T2F_C4F_N3F_V3F:
+      glVertexAttribPointer(0, 2, GL_FLOAT, GL_FALSE, getStride(), (void *)(0 * sizeof(float)));
+      glVertexAttribPointer(1, 4, GL_FLOAT, GL_FALSE, getStride(), (void *)(2 * sizeof(float)));
+      glVertexAttribPointer(2, 3, GL_FLOAT, GL_FALSE, getStride(), (void *)(9 * sizeof(float)));
+      break;
+    case NODES:
+      glVertexAttribPointer(0, 4, GL_UNSIGNED_BYTE, GL_TRUE, getStride(), (void *)(0)); // color
+      // skip normal
+      glVertexAttribPointer(1, 3, GL_FLOAT, GL_FALSE, getStride(), (void *)(8)); // position
+      glVertexAttribPointer(2, 1, GL_FLOAT, GL_FALSE, getStride(), (void *)(20)); // age
+      glVertexAttribPointer(3, 1, GL_FLOAT, GL_FALSE, getStride(), (void *)(24)); // size
+      glVertexAttribPointer(4, 2, GL_UNSIGNED_SHORT, GL_FALSE, getStride(), (void *)(28));
+      break;
+    case BILLBOARDS:
+      glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, getStride(), (void *)(0 * sizeof(float)));
+      glVertexAttribPointer(1, 2, GL_HALF_FLOAT, GL_FALSE, getStride(), (void *)(3 * sizeof(float)));
+      glVertexAttribPointer(2, 2, GL_HALF_FLOAT, GL_FALSE, getStride(), (void *)(4 * sizeof(float)));
+      break;
+    case EDGES:
+      glVertexAttribPointer(0, 4, GL_UNSIGNED_BYTE, GL_TRUE, getStride(), (void *)(0 * sizeof(float)));
+      glVertexAttribPointer(1, 3, GL_FLOAT, GL_FALSE, getStride(), (void *)(1 * sizeof(float)));
+      glVertexAttribPointer(2, 1, GL_FLOAT, GL_FALSE, getStride(), (void *)(4 * sizeof(float)));
+      glVertexAttribPointer(3, 1, GL_FLOAT, GL_FALSE, getStride(), (void *)(5 * sizeof(float)));
+      break;
+    case ARCS_2D:
+      glVertexAttribPointer(0, 4, GL_UNSIGNED_BYTE, GL_TRUE, getStride(), (void *)(0 * sizeof(float)));
+      glVertexAttribPointer(1, 2, GL_FLOAT, GL_FALSE, getStride(), (void *)(1 * sizeof(float)));
+      break;
+    }  
+  }
+
+  if (indexVbo) {
+    // assert(num_indices > 0);
+    glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, indexVbo);
+  }
+}
+
+void
+VBO::unbind() {
+  if (vbo) {
+    glBindBuffer(GL_ARRAY_BUFFER, 0);
+  }
+  if (indexVbo) {
+    glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, 0);
+  }
+  if (texture.get()) {
+    glBindTexture(GL_TEXTURE_2D, 0);
+  }
+}
+
+void
+VBO::upload(DataType type, const void * ptr, size_t size) {
+  // assert(size > 0);
+  
+  data_type = type;
+  switch (type) {
+  case VBO::C4F_N3F_V3F: stride = 10 * sizeof(float); break;
+  case VBO::T2F_N3F_V3F: stride = 8 * sizeof(float); break;
+  case VBO::T2F_C4F_N3F_V3F: stride = 12 * sizeof(float); break;
+  case VBO::NODES: stride = 8 * sizeof(float); break;
+  case VBO::BILLBOARDS: stride = 5 * sizeof(float); break;
+  case VBO::EDGES: stride = sizeof(line_data_s); break;
+  case VBO::ARCS_2D: stride = sizeof(arc_data_2d_s); break;
+  }
+  assert(sizeof(line_data_s) == 6 * 4);
+  
+  num_elements = size / stride;
+  if (!vao) {
+    // glGenVertexArrays(1, &vao);
+    // glBindVertexArray(vao);
+  }
+  if (!vbo) {
+    glGenBuffers(1, &vbo);
+  }
+  glBindBuffer(GL_ARRAY_BUFFER, vbo);
+  glBufferData(GL_ARRAY_BUFFER, size, ptr, GL_DYNAMIC_DRAW);
+  glBindBuffer(GL_ARRAY_BUFFER, 0);
+}
+
+void
+VBO::uploadIndices(const void * ptr, size_t size) {
+  assert(size > 0);
+
+  if (!indexVbo) {
+    glGenBuffers(1, &indexVbo);
+  }
+  num_indices = size / 4;
+  glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, indexVbo);
+  glBufferData(GL_ELEMENT_ARRAY_BUFFER, size, ptr, GL_DYNAMIC_DRAW);
+  glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, 0);
+}
+
+void
+VBO::draw(DrawType type, const vector<int> & _indices) {
+  cerr << "wrong draw\n";
+  bind();
+  const int & first_index = _indices.front();
+  glDrawElements(getGLDrawType(type), _indices.size(), GL_UNSIGNED_INT, &first_index);
+  unbind();
+}
+
+void
+VBO::draw(DrawType type) {
+  if (!type) type = default_draw_type;
+  bind();
+  if (!num_indices) {
+    glDrawArrays(getGLDrawType(type), 0, num_elements);
+  } else {
+    glDrawElements(getGLDrawType(type), num_indices, GL_UNSIGNED_INT, 0);
+  }
+  unbind();
+}
+
+void
+VBO::quad2d(float x1, float y1,
+	    float x2, float y2,
+	    float x3, float y3,
+	    float x4, float y4) {
+  default_draw_type = TRIANGLE_FAN;
+  vbo_data_s * data = new vbo_data_s[4];
+  unsigned int * indices = new unsigned int[4];
+  data[0] = { glm::vec2(0, 0), glm::vec3(0, 0, 1), glm::vec3(x1, y1, 0) };
+  data[1] = { glm::vec2(0, 1), glm::vec3(0, 0, 1), glm::vec3(x2, y2, 0) };
+  data[2] = { glm::vec2(1, 1), glm::vec3(0, 0, 1), glm::vec3(x3, y3, 0) };
+  data[3] = { glm::vec2(1, 0), glm::vec3(0, 0, 1), glm::vec3(x4, y4, 0) };
+  upload(T2F_N3F_V3F, data, 4 * sizeof(vbo_data_s));
+  indices[0] = 0;
+  indices[1] = 1;
+  indices[2] = 2;
+  indices[3] = 3;
+  uploadIndices(indices, 4 * sizeof(unsigned int));
+  delete[] indices;
+  delete[] data;
+}
+
+void
+VBO::quad2d(float x1, float y1, float tx1, float ty1,
+	    float x2, float y2, float tx2, float ty2,
+	    float x3, float y3, float tx3, float ty3,
+	    float x4, float y4, float tx4, float ty4) {
+  default_draw_type = TRIANGLE_FAN;
+  vbo_data_s * data = new vbo_data_s[4];
+  unsigned int * indices = new unsigned int[4];
+  data[0] = { glm::vec2(tx1, ty1), glm::vec3(0, 0, 1), glm::vec3(x1, y1, 0) };
+  data[1] = { glm::vec2(tx2, ty2), glm::vec3(0, 0, 1), glm::vec3(x2, y2, 0) };
+  data[2] = { glm::vec2(tx3, ty3), glm::vec3(0, 0, 1), glm::vec3(x3, y3, 0) };
+  data[3] = { glm::vec2(tx4, ty4), glm::vec3(0, 0, 1), glm::vec3(x4, y4, 0) };
+  upload(T2F_N3F_V3F, data, 4 * sizeof(vbo_data_s));
+  indices[0] = 0;
+  indices[1] = 1;
+  indices[2] = 2;
+  indices[3] = 3;
+  uploadIndices(indices, 4 * sizeof(unsigned int));
+  delete[] indices;
+  delete[] data;
+}
+
+void
+VBO::sphere(float radius, unsigned int u, unsigned int v) {
+  default_draw_type = TRIANGLES;
+  
+  vbo_data_s * data = new vbo_data_s[u * v];
+  unsigned int * indices = new unsigned int[6 * u * v];
+  
+  unsigned int in = 0, vn = 0;
+  for (unsigned int i = 0; i < u; i++) {
+    for (unsigned int j = 0; j < v; j++) {
+      float tx = (float)i / (u - 1), ty = (float)j / (v - 1);
+      float i2 = (float)i / u * 2 * M_PI, j2 = ((float)j / v - 0.5f) * M_PI;
+      float x = -radius * cos(j2) * cos(i2);
+      float y = -radius * sin(j2);
+      float z = radius * cos(j2) * sin(i2);
+      
+      indices[in++] = i * v + j;
+      indices[in++] = i * v + (j + 1) % v;
+      indices[in++] = ((i + 1) % u) * v + (j + 1) % v;
+      indices[in++] = i * v + j;
+      indices[in++] = ((i + 1) % u) * v + (j + 1) % v;
+      indices[in++] = ((i + 1) % u) * v + j;
+      
+      data[vn++] = { glm::vec2(tx, ty), glm::vec3(0, 0, 1), glm::vec3(x, y, z) };
+    }
+  }
+  
+  uploadIndices(indices, in * sizeof(unsigned int));
+  upload(T2F_N3F_V3F, data, vn * sizeof(vbo_data_s));  
+
+  delete[] indices;
+  delete[] data;
+}
+
+void
+VBO::ring(float outer_radius, float inner_radius, unsigned int n, float dx, float dy) {
+  default_draw_type = TRIANGLE_STRIP;
+  
+  vbo_data_s * data = new vbo_data_s[2 * n];
+  unsigned int * indices = new unsigned int[2 * n];
+    
+  unsigned int vn = 0, in = 0;
+  for (unsigned int i = 0; i < n; i++) {
+    float a = 2 * M_PI / (n - 1);
+    float x1 = outer_radius * cos(a) + dx, y1 = outer_radius * sin(a) + dy;
+    float x2 = inner_radius * cos(a) + dx, y2 = inner_radius * sin(a) + dy;
+
+    if (i % 2 == 0) {
+      indices[in++] = vn + 0;
+      indices[in++] = vn + 1;
+    } else {
+      indices[in++] = vn + 1;
+      indices[in++] = vn + 0;
+    }
+    
+    data[vn++] = { glm::vec2(0.0f, 0.0f), glm::vec3(0.0f, 0.0f, 1.0f), glm::vec3(x1, y1, 0.0f) };
+    data[vn++] = { glm::vec2(0.0f, 0.0f), glm::vec3(0.0f, 0.0f, 1.0f), glm::vec3(x2, y2, 0.0f) };    
+  }
+
+  cerr << "vbo ring vn = " << vn << ", in = " << in << endl;
+  
+  upload(T2F_N3F_V3F, data, vn * sizeof(vbo_data_s));  
+  uploadIndices(indices, in * sizeof(unsigned int));
+  
+  delete[] data;
+  delete[] indices;
+}
