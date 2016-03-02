d2a588654e30f1878daf8fd3a9aa1cacd2c69ed2
Mikael Rekola <mikael.rekola@gmail.com>
2016-03-02 18:49:55 +0200

    remove unnused vbo types, remove normal from node sprite VBO, add node billboards, add colors to label billboards

--- src/VBO.cpp
+++ src/VBO.cpp
@@ -37,31 +37,31 @@ VBO::bind() {
   if (vbo) {
     glBindBuffer(GL_ARRAY_BUFFER, vbo);
     switch (getDataType()) {
-    case C4F_N3F_V3F:
-      glVertexAttribPointer(0, 4, GL_FLOAT, GL_FALSE, getStride(), (void *)(0 * sizeof(float)));
-      glVertexAttribPointer(1, 3, GL_FLOAT, GL_FALSE, getStride(), (void *)(7 * sizeof(float)));
-      break;
     case T2F_N3F_V3F:
       glVertexAttribPointer(0, 2, GL_FLOAT, GL_FALSE, getStride(), (void *)(0 * sizeof(float)));
       glVertexAttribPointer(1, 3, GL_FLOAT, GL_FALSE, getStride(), (void *)(5 * sizeof(float)));
       break;
-    case T2F_C4F_N3F_V3F:
-      glVertexAttribPointer(0, 2, GL_FLOAT, GL_FALSE, getStride(), (void *)(0 * sizeof(float)));
-      glVertexAttribPointer(1, 4, GL_FLOAT, GL_FALSE, getStride(), (void *)(2 * sizeof(float)));
-      glVertexAttribPointer(2, 3, GL_FLOAT, GL_FALSE, getStride(), (void *)(9 * sizeof(float)));
-      break;
     case NODES:
       glVertexAttribPointer(0, 4, GL_UNSIGNED_BYTE, GL_TRUE, getStride(), (void *)(0)); // color
-      // skip normal
-      glVertexAttribPointer(1, 3, GL_FLOAT, GL_FALSE, getStride(), (void *)(8)); // position
-      glVertexAttribPointer(2, 1, GL_FLOAT, GL_FALSE, getStride(), (void *)(20)); // age
-      glVertexAttribPointer(3, 1, GL_FLOAT, GL_FALSE, getStride(), (void *)(24)); // size
-      glVertexAttribPointer(4, 2, GL_UNSIGNED_SHORT, GL_FALSE, getStride(), (void *)(28));
+      glVertexAttribPointer(1, 3, GL_FLOAT, GL_FALSE, getStride(), (void *)(4)); // position
+      glVertexAttribPointer(2, 1, GL_FLOAT, GL_FALSE, getStride(), (void *)(16)); // age
+      glVertexAttribPointer(3, 1, GL_FLOAT, GL_FALSE, getStride(), (void *)(20)); // size
+      glVertexAttribPointer(4, 2, GL_UNSIGNED_SHORT, GL_FALSE, getStride(), (void *)(24)); // texture / flags
+      break;
+    case NODE_BILLBOARDS:
+      glVertexAttribPointer(0, 4, GL_UNSIGNED_BYTE, GL_TRUE, getStride(), (void *)(0)); // color
+      glVertexAttribPointer(1, 3, GL_FLOAT, GL_FALSE, getStride(), (void *)(4)); // center position
+      glVertexAttribPointer(2, 1, GL_FLOAT, GL_FALSE, getStride(), (void *)(16)); // age
+      glVertexAttribPointer(3, 1, GL_FLOAT, GL_FALSE, getStride(), (void *)(20)); // size
+      glVertexAttribPointer(4, 1, GL_FLOAT, GL_FALSE, getStride(), (void *)(24)); // scaling
+      glVertexAttribPointer(5, 2, GL_UNSIGNED_SHORT, GL_FALSE, getStride(), (void *)(28)); // texture / flags
       break;
     case BILLBOARDS:
       glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, getStride(), (void *)(0 * sizeof(float)));
       glVertexAttribPointer(1, 2, GL_HALF_FLOAT, GL_FALSE, getStride(), (void *)(3 * sizeof(float)));
       glVertexAttribPointer(2, 2, GL_HALF_FLOAT, GL_FALSE, getStride(), (void *)(4 * sizeof(float)));
+      glVertexAttribPointer(3, 4, GL_UNSIGNED_BYTE, GL_TRUE, getStride(), (void *)(5 * sizeof(float)));
+      glVertexAttribPointer(4, 4, GL_UNSIGNED_BYTE, GL_TRUE, getStride(), (void *)(6 * sizeof(float)));
       break;
     case EDGES:
       glVertexAttribPointer(0, 4, GL_UNSIGNED_BYTE, GL_TRUE, getStride(), (void *)(0 * sizeof(float)));
@@ -106,16 +106,20 @@ VBO::upload(DataType type, const void * ptr, size_t size) {
   
   data_type = type;
   switch (type) {
+#if 0
   case VBO::C4F_N3F_V3F: stride = 10 * sizeof(float); break;
-  case VBO::T2F_N3F_V3F: stride = 8 * sizeof(float); break;
   case VBO::T2F_C4F_N3F_V3F: stride = 12 * sizeof(float); break;
-  case VBO::NODES: stride = 8 * sizeof(float); break;
-  case VBO::BILLBOARDS: stride = 5 * sizeof(float); break;
+#endif
+  case VBO::T2F_N3F_V3F: stride = 8 * sizeof(float); break;
+  case VBO::NODES: stride = sizeof(node_vbo_s); break;
+  case VBO::NODE_BILLBOARDS: stride = sizeof(node_billboard_vbo_s); break; // ?
+  case VBO::BILLBOARDS: stride = sizeof(billboard_data_s); break;
   case VBO::EDGES: stride = sizeof(line_data_s); break;
   case VBO::ARCS_2D: stride = sizeof(arc_data_2d_s); break;
   case VBO::ARCS_3D: stride = sizeof(arc_data_3d_s); break;
   }
   assert(sizeof(line_data_s) == 6 * 4);
+  assert(sizeof(node_vbo_s) == 7 * 4);
   
   num_elements = size / stride;
   if (!vao) {
