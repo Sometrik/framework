be4da043559c7e0ac117e425a22e5011b292c86c
Mikael Rekola <mikael.rekola@gmail.com>
2016-08-16 14:29:03 +0300

    add simple_sprite_shader.glsl

---
+++ BombDefender/shaders/simple_sprite_shader.glsl
@@ -0,0 +1,29 @@
+precision highp float;
+
+uniform mat4 proj_mv_matrix;
+uniform sampler2D s_texture;
+uniform float alpha;
+
+#ifdef VERTEX
+
+attribute vec2 a_texCoord;
+attribute vec3 a_position;
+varying vec2 v_texCoord;
+
+void main() {
+  gl_Position = proj_mv_matrix * vec4(a_position, 1.0);
+  v_texCoord = a_texCoord;
+}
+
+#endif
+
+#ifdef FRAGMENT
+
+varying vec2 v_texCoord;
+// out vec4 o_fragColor;
+
+void main() {
+  gl_FragColor = alpha * texture2D( s_texture, v_texCoord );
+}
+
+#endif
