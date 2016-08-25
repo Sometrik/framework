0c20d369326b0a7a44379e6ab3de75bd96b96c2b
Mikael Rekola <mikael.rekola@gmail.com>
2016-08-25 12:33:15 +0300

    add width, height and accessors

--- include/Sprite.h
+++ include/Sprite.h
@@ -13,8 +13,18 @@ class Sprite {
   canvas::TextureRef getTexture() const { return texture; }
   void setTexture(canvas::TextureRef _texture) { texture = _texture; }
 
+  float getX() const { return x; }
+  float getY() const { return y; }
+  float getWidth() const { return width; }
+  float getHeight() const { return height; }
+
+  void setX(float _x) { x = _x; }
+  void setY(float _y) { y = _y; }
+  void setWidth(float _width) { width = _width; }
+  void setHeight(float _height) { height = _height; }
+  
  private:
-  float x = 0, y = 0;
+  float x = 0, y = 0, width = 0, height = 0;
   canvas::TextureRef texture;
 };
 
