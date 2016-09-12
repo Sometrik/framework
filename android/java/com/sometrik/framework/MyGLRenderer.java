1c34bcb9d543c6bb025b169c4e8cfd791920af1d
MSuni <mikko.suni@hotmail.com>
2016-09-12 15:24:00 +0300

    Remove unused code

--- android/java/com/sometrik/framework/MyGLRenderer.java
+++ android/java/com/sometrik/framework/MyGLRenderer.java
@@ -77,18 +77,4 @@ public class MyGLRenderer implements GLSurfaceView.Renderer {
 
   }
 
-  // Wanhaa
-  public static int loadShader(int type, String shaderCode) {
-
-    // create a vertex shader type (GLES20.GL_VERTEX_SHADER)
-    // or a fragment shader type (GLES20.GL_FRAGMENT_SHADER)
-    int shader = GLES20.glCreateShader(type);
-
-    // add the source code to the shader and compile it
-    GLES20.glShaderSource(shader, shaderCode);
-    GLES20.glCompileShader(shader);
-
-    return shader;
-  }
-
 }
