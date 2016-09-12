8768060451ba90207162d36dceb91b6a1307073c
Mikael Rekola <mikael.rekola@gmail.com>
2016-09-12 06:45:17 +0300

    add abstract method createEventLoop()

--- include/FWPlatformBase.h
+++ include/FWPlatformBase.h
@@ -18,6 +18,7 @@
 #include <memory>
 
 class FWContextBase;
+class EventLoop;
 
 namespace canvas {
   class ContextFactory;
@@ -44,6 +45,7 @@ class FWPlatformBase {
   
   bool createContext(FWContextBase * context, const char * title, int requested_width, int requested_height);
 
+  virtual std::shared_ptr<EventLoop> createEventLoop() { return std::shared_ptr<EventLoop>(0); }
   virtual bool createWindow(FWContextBase * context, const char * title) = 0;
   virtual void showMessageBox(const std::string & title, const std::string & message) = 0;
   virtual std::string showTextEntryDialog(const std::string & message) = 0;
