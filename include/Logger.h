4afc9589eb3695342462a30332929f36049df0ab
Mikael Rekola <mikael.rekola@gmail.com>
2016-09-26 02:06:24 +0300

    add Logger.h

---
+++ include/Logger.h
@@ -0,0 +1,24 @@
+#ifndef _LOGGER_H_
+#define _LOGGER_H_
+
+#include <iostream>
+
+class Logger {
+ public:
+  Logger() { }
+  virtual ~Logger() { }
+  
+  virtual void println(const char * s) = 0;
+  void println(const std::string & s) { println(s.c_str()); }  
+};
+
+class BasicLogger {
+ public:
+  BasicLogger() { }
+  
+  void println(const char * s) override {
+    std::cerr << s << std::endl;
+  }
+};
+
+#endif
