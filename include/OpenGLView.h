0ef2baf9867350a6d45c4a66e6928eec9e91c1ea
MSuni <mikko.suni@hotmail.com>
2016-10-27 15:22:40 +0300

    Add OpenGLView class

---
+++ include/OpenGLView.h
@@ -0,0 +1,21 @@
+#ifndef _OPENGLVIEW_H_
+#define _OPENGLVIEW_H_
+
+#include <FWViewBase.h>
+
+class OpenGLView : public FWViewBase {
+ public:
+  
+
+
+  OpenGLView() : FWViewBase(){
+
+  }
+
+
+
+
+ protected:
+};
+
+#endif
