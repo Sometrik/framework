ee1b995e2df3a13b0773804a7286f983bc25ec30
Mikael Rekola <mikael.rekola@gmail.com>
2016-11-01 17:06:58 +0200

    add initializer, remove useless constructor parameter, add resize handler and sizes

--- include/OpenGLView.h
+++ include/OpenGLView.h
@@ -2,20 +2,43 @@
 #define _OPENGLVIEW_H_
 
 #include <FWViewBase.h>
+#include <Message.h>
 
 class OpenGLView : public FWViewBase {
  public:
   
-  OpenGLView(FWPlatformBase * platform) : FWViewBase(platform){
+  OpenGLView(){
 
   }
   ~OpenGLView(){
 
   }
 
+  void initialize(FWPlatformBase * _platform) override {
+    FWViewBase::initialize(_platform);
+    sendMessage(Message(Message::CREATE_OPENGL_VIEW, getId()));
+  }
+
+   bool onResize(int _logical_width, int _logical_height, int _actual_width, int _actual_height) {
+     setWindowSize(_logical_width, _logical_height, _actual_width, _actual_height);
+     return false;
+   }
+
+  int getLogicalWidth() const { return logical_width; }
+  int getLogicalHeight() const { return logical_height; }
+  int getActualWidth() const { return actual_width; }
+  int getActualHeight() const { return actual_height; }
+
+  void setWindowSize(int _logical_width, int _logical_height, int _actual_width, int _actual_height) {
+    logical_width = _logical_width;
+    logical_height = _logical_height;
+    actual_width = _actual_width;
+    actual_height = _actual_height;
+  }
 
 
  protected:
+  int logical_width = 0, logical_height = 0, actual_width = 0, actual_height = 0;
 };
 
 #endif
