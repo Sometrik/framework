3cf66728cf654b371bb132c32410238f3679edbf
Mikael Rekola <mikael.rekola@gmail.com>
2016-11-01 20:48:50 +0200

    remove OpenGL related code

--- include/FWContextBase.h
+++ include/FWContextBase.h
@@ -3,48 +3,12 @@
 
 #include <Element.h>
 
-#ifdef __APPLE__
-#include <OpenGLES/ES3/gl.h>
-#else
-#include <GLES3/gl3.h>
-#include <EGL/egl.h>
-#include <EGL/eglext.h>
-#endif
-
-#include <TouchEvent.h>
-
 class FWContextBase : public Element {
 public:
   FWContextBase() { }
     
   bool createWindow(const char * title, int requested_width, int requested_height);
-  
-  bool onResize(int _logical_width, int _logical_height, int _actual_width, int _actual_height) override {
-    logical_width = _logical_width;
-    logical_height = _logical_height;
-    actual_width = _actual_width;
-    actual_height = _actual_height;
-    return false;
-  }
-
   virtual bool loadEvents() { return false; }
-
-  int getLogicalWidth() const { return logical_width; }
-  int getLogicalHeight() const { return logical_height; }
-  int getActualWidth() const { return actual_width; }
-  int getActualHeight() const { return actual_height; }
-
-  void setWindowSize(int _logical_width, int _logical_height, int _actual_width, int _actual_height) {
-    logical_width = _logical_width;
-    logical_height = _logical_height;
-    actual_width = _actual_width;
-    actual_height = _actual_height;
-  }
-
- protected:
-    
- private:
-  int logical_width = 0, logical_height = 0, actual_width = 0, actual_height = 0;
 };
 
 #endif
