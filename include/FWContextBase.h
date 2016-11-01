12a01c7a9483dbe953ebc9f5ebb7fa58027a5e87
Mikael Rekola <mikael.rekola@gmail.com>
2016-11-01 20:06:11 +0200

    remove references to platform, remove useless include

--- include/FWContextBase.h
+++ include/FWContextBase.h
@@ -12,17 +12,13 @@
 #endif
 
 #include <TouchEvent.h>
-#include <FWViewBase.h>
-
-class FWPlatformBase;
 
 class FWContextBase : public Element {
 public:
-  FWContextBase(FWPlatformBase * _platform) : platform(_platform) { }
+  FWContextBase() { }
     
   bool createWindow(const char * title, int requested_width, int requested_height);
-
-  virtual bool Init() = 0;
+  
   bool onResize(int _logical_width, int _logical_height, int _actual_width, int _actual_height) override {
     logical_width = _logical_width;
     logical_height = _logical_height;
@@ -38,9 +34,6 @@ public:
   int getActualWidth() const { return actual_width; }
   int getActualHeight() const { return actual_height; }
 
-  FWPlatformBase & getPlatform() { return *platform; }
-  const FWPlatformBase & getPlatform() const { return *platform; }
-
   void setWindowSize(int _logical_width, int _logical_height, int _actual_width, int _actual_height) {
     logical_width = _logical_width;
     logical_height = _logical_height;
@@ -52,7 +45,6 @@ public:
     
  private:
   int logical_width = 0, logical_height = 0, actual_width = 0, actual_height = 0;
-  FWPlatformBase * platform;
 };
 
 #endif
