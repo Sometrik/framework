2364fdee72f3ac1f58a1d15a094bfe5409c0469d
Mikael Rekola <mikael.rekola@gmail.com>
2016-11-01 20:00:52 +0200

    remove unnecessary methods

--- include/FWContextBase.h
+++ include/FWContextBase.h
@@ -1,6 +1,8 @@
 #ifndef _FWCONTEXTBASE_H_
 #define _FWCONTEXTBASE_H_
 
+#include <Element.h>
+
 #ifdef __APPLE__
 #include <OpenGLES/ES3/gl.h>
 #else
@@ -16,29 +18,18 @@ class FWPlatformBase;
 
 class FWContextBase : public Element {
 public:
- FWContextBase(FWPlatformBase * _platform) : platform(_platform) { }
-  virtual ~FWContextBase() { }
+  FWContextBase(FWPlatformBase * _platform) : platform(_platform) { }
     
   bool createWindow(const char * title, int requested_width, int requested_height);
 
   virtual bool Init() = 0;
-  virtual void onDraw() = 0;
-  virtual void onShutdown() = 0;
-  virtual void onMemoryWarning() { }
-  virtual void onCmdLine(int argc, char *argv[]) { }
-
-  virtual bool onKeyPress(char c, double timestamp, int x, int y) { return false; }
-  virtual bool onUpdate(double timestamp) { return false; }
-  virtual bool onResize(int _logical_width, int _logical_height, int _actual_width, int _actual_height) {
+  bool onResize(int _logical_width, int _logical_height, int _actual_width, int _actual_height) override {
     logical_width = _logical_width;
     logical_height = _logical_height;
     actual_width = _actual_width;
     actual_height = _actual_height;
     return false;
   }
-
-  virtual bool flushTouches(int mode, double timestamp) { return false; }
-  virtual bool onShake(double timestamp) { return false; }
 
   virtual bool loadEvents() { return false; }
 
