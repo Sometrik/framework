a6697bc909c12766fb9c58f98540492a4b4c1c41
Mikael Rekola <mikael.rekola@gmail.com>
2016-11-01 17:07:53 +0200

    remove useless code

--- include/FWViewBase.h
+++ include/FWViewBase.h
@@ -1,60 +1,15 @@
 #ifndef _FWVIEW_H_
 #define _FWVIEW_H_
-#include <element.h>
-#include <widget.h>
+#include <Element.h>
 
-class FWPlatformBase;
-
-class FWViewBase {
+class FWViewBase : public Element{
  public:
   
-  FWViewBase(FWPlatformBase * _platform) : platform(_platform) {
+  FWViewBase() {
 
   }
-  virtual ~FWViewBase() { }
-
-  bool createWindow(const char * title, int requested_width, int requested_height);
-
-    virtual void onDraw() = 0;
-    virtual void onCmdLine(int argc, char *argv[]) { }
-
-    virtual bool onKeyPress(char c, double timestamp, int x, int y) { return false; }
-    virtual bool onUpdate(double timestamp) { return false; }
-    virtual bool onResize(int _logical_width, int _logical_height, int _actual_width, int _actual_height) {
-      logical_width = _logical_width;
-      logical_height = _logical_height;
-      actual_width = _actual_width;
-      actual_height = _actual_height;
-      return false;
-    }
-
-    virtual bool touchesBegin(int x, int y, double timestamp, long identifier) { return false; }
-    virtual bool touchesEnded(int x, int y, double timestamp, long identifier) { return false; }
-    virtual bool touchesMoved(int x, int y, double timestamp, long identifier) { return false; }
-    virtual bool flushTouches(int mode, double timestamp) { return false; }
-    virtual bool onShake(double timestamp) { return false; }
-
-    virtual bool loadEvents() { return false; }
-
-    int getLogicalWidth() const { return logical_width; }
-    int getLogicalHeight() const { return logical_height; }
-    int getActualWidth() const { return actual_width; }
-    int getActualHeight() const { return actual_height; }
-
-    void setWindowSize(int _logical_width, int _logical_height, int _actual_width, int _actual_height) {
-      logical_width = _logical_width;
-      logical_height = _logical_height;
-      actual_width = _actual_width;
-      actual_height = _actual_height;
-    }
-
-    FWPlatformBase & getPlatform() { return *platform; }
-
-    virtual void showView() = 0;
 
 protected:
-  int logical_width = 0, logical_height = 0, actual_width = 0, actual_height = 0;
-  FWPlatformBase * platform;
 };
 
 #endif
