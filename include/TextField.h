6decc752ee94fe9b7c7829034f897bc90d85c9e9
Mikael Rekola <mikael.rekola@gmail.com>
2016-11-02 19:19:07 +0200

    add TextField.h

---
+++ include/TextField.h
@@ -0,0 +1,22 @@
+#ifndef _TEXTFIELD_H_
+#define _TEXTFIELD_H_
+
+#include <Element.h>
+#include <Message.h>
+
+class TextField : public Element {
+ public:
+ TextField() { }
+
+  void initialize(FWPlatformBase * _platform) override {
+    FWViewBase::initialize(_platform);
+    sendMessage(Message(Message::CREATE_TEXTFIELD, getId(), getParentId()));
+  }
+
+  const std::string & getValue() { return value; }
+			       
+ private:
+  std::string value;
+};
+
+#endif
