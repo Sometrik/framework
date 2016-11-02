0e1388b6980838e666b4aa29609a16f4a6598b9f
Mikael Rekola <mikael.rekola@gmail.com>
2016-11-02 10:51:25 +0200

    rename FWContextBase to FWApplication. Fixes #29

--- include/FWApplication.h
+++ include/FWApplication.h
@@ -1,12 +1,12 @@
-#ifndef _FWCONTEXTBASE_H_
-#define _FWCONTEXTBASE_H_
+#ifndef _FWAPPLICATION_H_
+#define _FWAPPLICATION_H_
 
 #include <Element.h>
 #include <Message.h>
 
-class FWContextBase : public Element {
+class FWApplication : public Element {
 public:
-  FWContextBase() { }
+  FWApplication(bool _full_screen = false) : full_screen(_full_screen) { }
     
   virtual bool loadEvents() { return false; }
   
@@ -15,6 +15,9 @@ public:
     m.setTextValue(s);
     sendMessage(m);
   }
+
+ private:
+  bool full_screen;
 };
 
 #endif
