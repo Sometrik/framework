d37bb04bd233fa54294e5cd739be573edf4b55e3
Mikael Rekola <mikael.rekola@gmail.com>
2016-11-02 19:18:48 +0200

    LinearLayout.h

---
+++ include/LinearLayout.h
@@ -0,0 +1,25 @@
+#ifndef _LINEARLAYOUT_H_
+#define _LINEARLAYOUT_H_
+
+#include <Element.h>
+#include <Message.h>
+
+#define FW_VERTICAL	1
+#define FW_HORIZONTAL	2
+
+class LinearLayout : public Element {
+ public:
+  LinearLayout(int _direction) : direction(_direction) { }
+
+  void initialize(FWPlatformBase * _platform) override {
+    FWViewBase::initialize(_platform);
+    Message m(Message::CREATE_LINEAR_LAYOUT, getId(), getParentId());
+    m.setValue(direction);
+    sendMessage(m);
+  }
+			       
+ private:
+  int direction = FW_VERTICAL;
+};
+
+#endif
