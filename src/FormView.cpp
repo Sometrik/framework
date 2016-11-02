7dfac197536f804cc926ed34c74234cf88caf3d1
Mikael Rekola <mikael.rekola@gmail.com>
2016-11-02 19:07:30 +0200

    remove FormView.cpp

--- src/FormView.cpp
+++
@@ -1,8 +0,0 @@
-#include <FormView.h>
-#include <Message.h>
-
-void
-FormView::initialize(FWPlatformBase * _platform) {
-  FWViewBase::initialize(_platform);
-  sendMessage(Message(Message::CREATE_FORM_VIEW, getId()));  
-}
