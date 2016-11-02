c8975c4cce123d9576833b7a0ce5c82474986b32
Mikael Rekola <mikael.rekola@gmail.com>
2016-11-02 19:07:44 +0200

    add error checker

--- src/OpenGLView.cpp
+++ src/OpenGLView.cpp
@@ -1,6 +1,19 @@
 #include <OpenGLView.h>
 #include <FWPlatformBase.h>
 #include <Message.h>
+#include <Logger.h>
+
+#include <sstream>
+
+#ifdef __APPLE__
+#include <OpenGLES/ES3/gl.h>
+#else
+#include <GLES3/gl3.h>
+#include <EGL/egl.h>
+#include <EGL/eglext.h>
+#endif
+
+using namespace std;
 
 void
 OpenGLView::initialize(FWPlatformBase * _platform) {
@@ -10,4 +23,29 @@ OpenGLView::initialize(FWPlatformBase * _platform) {
   logical_height = int(_platform->getDisplayHeight() / _platform->getDisplayScale());
   actual_width = _platform->getDisplayWidth();
   actual_height = _platform->getDisplayHeight();    
+}
+
+void
+OpenGLView::checkGLError() {
+  GLenum errLast = GL_NO_ERROR;
+  for (;;) {
+    GLenum err = glGetError();
+    if (err == GL_NO_ERROR) {
+      return;
+    }
+
+    // normally the error is reset by the call to glGetError() but if
+    // glGetError() itself returns an error, we risk looping forever here
+    // so check that we get a different error than the last time
+    if (err == errLast) {
+      getPlatform().getLogger().println("OpenGL error state couldn't be reset.");
+      return;
+    }
+
+    errLast = err;
+
+    ostringstream s;
+    s << "got OpenGL error " << err;
+    getPlatform().getLogger().println(s.str());
+  }
 }
