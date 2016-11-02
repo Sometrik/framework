7678ce0d4f8982f214068a4f4a9f510befc4c34f
Mikael Rekola <mikael.rekola@gmail.com>
2016-11-02 20:22:36 +0200

    refactor

--- include/TouchEvent.h
+++ include/TouchEvent.h
@@ -5,28 +5,24 @@
 
 class TouchEvent : public EventBase {
 public:
-  enum Type{
-      ACTION_DOWN, ACTION_MOVE, ACTION_UP
-    };
-  TouchEvent(Type _type, int _x, int _y, double _timestamp, int _identifier) : type(_type), x(_x), y(_y), time(_timestamp), id(_identifier){
+  enum Type {
+    ACTION_DOWN, ACTION_MOVE, ACTION_UP
+  };
+  TouchEvent(Type _type, int _x, int _y, double _timestamp, int _identifier)
+    : EventBase(0), type(_type), x(_x), y(_y), time(_timestamp), identifier(_identifier) { }
 
-  }
-  ~TouchEvent(){
-
-  }
-
-  Type getType(){ return type; }
-  int getX(){ return x; }
-  int getY(){ return y; }
-  double timestamp(){ return time; }
-  long identifier(){ return id; }
+  Type getType() { return type; }
+  int getX() { return x; }
+  int getY() { return y; }
+  double getTimestamp() { return time; }
+  long getIdentifier() { return identifier; }
 
  private:
   Type type;
   int x;
   int y;
   double time;
-  int id;
+  int identifier;
 };
 
 #endif
