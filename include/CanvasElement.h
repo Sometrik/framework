5a37f6b56e025ab495ac33db17377b0dfab92036
Mikael Rekola <mikael.rekola@gmail.com>
2016-11-03 11:48:29 +0200

    add texture, position and size fields

--- include/CanvasElement.h
+++ include/CanvasElement.h
@@ -1,10 +1,21 @@
 #ifndef _CANVASELEMENT_H_
 #define _CANVASELEMENT_H_
 
+#include <Element.h>
+#include <TextureRef.h>
+ 
 class CanvasElement : public Element {
  public:
   CanvasElement() { }
+
+  void setX(float _x) { x = _x; }
+  void setY(float _y) { y = _y; }
+  void setWidth(float _width) { width = _width; }
+  void setHeight(float _height) { height = _height; }
   
+ private:
+  float x = 0, y = 0, width = 0, height = 0;
+  canvas::TextureRef texture;
 };
 
 #endif
