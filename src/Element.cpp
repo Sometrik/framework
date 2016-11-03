857b57b887c887eba7d04bd16b62e83421aa75ef
Mikael Rekola <mikael.rekola@gmail.com>
2016-11-03 23:16:32 +0200

    add addChild() for labels

--- src/Element.cpp
+++ src/Element.cpp
@@ -1,5 +1,8 @@
 #include <Element.h>
 #include <FWPlatform.h>
+#include <TextLabel.h>
+
+using namespace std;
 
 void
 Element::initialize(FWPlatform * _platform) {
@@ -7,9 +10,12 @@ Element::initialize(FWPlatform * _platform) {
   id = platform->getNextElementId();
 }
 
-
 void
 Element::sendMessage(const Message & message){
   platform->sendMessage(message);
 }
 
+Element &
+Element::addChild(const std::string & text) {
+  return addChild(make_shared<TextLabel>(text));
+}
