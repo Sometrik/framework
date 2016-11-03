045734ee11532b2782a974df777225f1ecd002db
Mikael Rekola <mikael.rekola@gmail.com>
2016-11-03 11:30:35 +0200

    reformat code, rename onDraw to onDrawEvent

--- include/Element.h
+++ include/Element.h
@@ -18,44 +18,43 @@ class Element {
 
   virtual void initialize(FWPlatform * _platform);
   virtual void initializeContent() { }
-  virtual void onDraw(DrawEvent & ev) { }
+
+  virtual void onDrawEvent(DrawEvent & ev) { }
+  virtual void onCommandEvent(CommandEvent & ev) { }
+  virtual bool onTouchEvent(TouchEvent & ev) { return false; }
+  
   virtual void onShutdown() { }
   virtual void onMemoryWarning() { }
-  virtual void onCommandEvent(CommandEvent & ev) { }
+  virtual void onCmdLine(int argc, char *argv[]) { }
+  virtual bool onKeyPress(char c, double timestamp, int x, int y) { return false; }
+  virtual bool onUpdate(double timestamp) { return false; }
+  virtual bool onResize(int _logical_width, int _logical_height, int _actual_width, int _actual_height) { return false; }
+  virtual bool flushTouches(int mode, double timestamp) { return false; }
+  virtual bool onShake(double timestamp) { return false; }
 
-      virtual void onCmdLine(int argc, char *argv[]) { }
+  Element & addChild(const std::shared_ptr<Element> & element) {
+    element->parent = this;
+    element->initialize(platform);
+    children.push_back(element);
+    element->initializeContent();
+    return *element;
+  }
 
-      virtual bool onKeyPress(char c, double timestamp, int x, int y) { return false; }
-      virtual bool onUpdate(double timestamp) { return false; }
-      virtual bool onResize(int _logical_width, int _logical_height, int _actual_width, int _actual_height) { return false; }
-
-      virtual bool onTouchEvent(TouchEvent & ev) { return false; }
-      virtual bool flushTouches(int mode, double timestamp) { return false; }
-      virtual bool onShake(double timestamp) { return false; }
-
-      Element & addChild(const std::shared_ptr<Element> & element) {
-	element->parent = this;
-        element->initialize(platform);
-        children.push_back(element);
-	element->initializeContent();
-	return *element;
-      }
-
-      int getId() const { return id; }
-      int getParentId() const { return parent ? parent->getId() : 0; }
-
-      void sendMessage(const Message & message);
-
-      FWPlatform & getPlatform() { return *platform; }
-      const FWPlatform & getPlatform() const { return *platform; }
-
-      std::shared_ptr<Element> getFirstChild() const {
-	if (!children.empty()) {
-	  return children.front();
-	} else {
-	  return std::shared_ptr<Element>(0);
-	}
-      }
+  int getId() const { return id; }
+  int getParentId() const { return parent ? parent->getId() : 0; }
+  
+  void sendMessage(const Message & message);
+  
+  FWPlatform & getPlatform() { return *platform; }
+  const FWPlatform & getPlatform() const { return *platform; }
+  
+  std::shared_ptr<Element> getFirstChild() const {
+    if (!children.empty()) {
+      return children.front();
+    } else {
+      return std::shared_ptr<Element>(0);
+    }
+  }
 
   std::vector<std::shared_ptr<Element> > & getChildren() { return children; }
   
