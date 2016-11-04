667b326ac00440a1eecc686fdd36e9e498ea12a9
Mikael Rekola <mikael.rekola@gmail.com>
2016-11-04 18:38:20 +0200

    add ElementHandle.h

---
+++ include/ElementHandle.h
@@ -0,0 +1,15 @@
+#ifndef _ELEMENTHANDLE_H_
+#define _ELEMENTHANDLE_H_
+
+class ElementHandle {
+ public:
+  ElementHandle(Element * _element) : element(_element) { }
+
+  void show() {
+    
+  }  
+ private:
+  
+};
+
+#endif
