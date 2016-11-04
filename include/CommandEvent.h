7f1f9b80c7b4d3cfb734b6005adb584e3c1f5e02
Mikael Rekola <mikael.rekola@gmail.com>
2016-11-04 18:39:50 +0200

    add accessors

--- include/CommandEvent.h
+++ include/CommandEvent.h
@@ -8,7 +8,10 @@ public:
   CommandEvent(int _element_id, int _value = 0) : element_id(_element_id), value(_value) { }
 
   void dispatch(Element & element) override;
-    
+
+  int getElementId() const { return element_id; }
+  int getValue() const { return value; }
+  
  private:
   int element_id, value;
 };
