2236240495918e4392f477f476fa27bea337eee6
Mikael Rekola <mikael.rekola@gmail.com>
2016-11-04 04:14:09 +0200

    add drawContent() method

--- include/CButton.h
+++ include/CButton.h
@@ -8,6 +8,15 @@ class CButton : public CanvasElement {
  public:
   CButton(const std::string & _label) : label(_label) { }
 
+#if 0
+  canvas::TextureRef drawContent() {
+    auto context = getContextFactory().createContext(width, height, canvas::InternalFormat::RGBA8, true);
+    context->font.size = 20;
+    context->fillText(label, width / 2, height / 2);
+    return canvas::OpenGLTexture::createTexture(context->getDefaultSurface());
+  }
+#endif
+
  private:
   std::string label;
   float highlight = 0.0f;
