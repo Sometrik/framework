fd24503dc7412890c484f8ab19a092b5f105c2af
Mikael Rekola <mikael.rekola@gmail.com>
2016-11-08 17:46:32 +0200

    modify appearance

--- include/CButton.h
+++ include/CButton.h
@@ -10,8 +10,12 @@ class CButton : public CanvasElement {
 
   canvas::TextureRef drawContent() override {
     auto context = getPlatform().createContextFactory()->createContext(width, height, canvas::InternalFormat::RGBA8, true);
+    context->fillStyle = "#e80";
+    context->fillRect(0, 0, width, height);
+
+    context->fillStyle = "#fff";
     context->font.size = 20;
-    context->fillText(label, width / 2, height / 2);
+    context->fillText(label, width / 2, height / 2);    
     return canvas::OpenGLTexture::createTexture(context->getDefaultSurface());
   }
 
