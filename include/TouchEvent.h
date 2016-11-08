767d4f60dc1770dad773a759b5b6437e82d83853
Mikael Rekola <mikael.rekola@gmail.com>
2016-11-08 20:18:15 +0200

    change coordinate types to float

--- include/TouchEvent.h
+++ include/TouchEvent.h
@@ -8,20 +8,19 @@ public:
   enum Type {
     ACTION_DOWN, ACTION_MOVE, ACTION_UP
   };
- TouchEvent(Type _type, int _x, int _y, double _timestamp, int _identifier)
+ TouchEvent(Type _type, float _x, float _y, double _timestamp, int _identifier)
    : EventBase(_timestamp), type(_type), x(_x), y(_y), identifier(_identifier) { }
 
   void dispatch(Element & element) override;
   
   Type getType() const { return type; }
-  int getX() const { return x; }
-  int getY() const { return y; }
+  float getX() const { return x; }
+  float getY() const { return y; }
   long getIdentifier() const { return identifier; }
 
  private:
   Type type;
-  int x;
-  int y;
+  float x, y;
   int identifier;
 };
 
