ba63581ba00174f560b0717553385f7793576e20
Mikael Rekola <mikael.rekola@gmail.com>
2016-11-09 02:16:07 +0200

    modify button apperance

--- include/CButton.h
+++ include/CButton.h
@@ -10,15 +10,22 @@ class CButton : public CanvasElement {
 
   canvas::TextureRef drawContent() override {
     auto context = getPlatform().createContextFactory()->createContext(width, height, canvas::InternalFormat::RGBA8, true);
+
+    float x = 0.0f, y = 0.0f, w = width - 1, h = height - 1, r = 5.0f;
+    context->moveTo(x+r, y);
+    context->arcTo(x+w, y,   x+w, y+h, r);
+    context->arcTo(x+w, y+h, x,   y+h, r);
+    context->arcTo(x,   y+h, x,   y,   r);
+    context->arcTo(x,   y,   x+w, y,   r);
     if (is_touched) {
-      context->fillStyle = "#e80";
-      context->fillRect(0, 0, width, height);
-    } else {
-      context->strokeStyle = "#e80";
-      context->strokeRect(0, 0, width, height);
+      context->fillStyle = "#a40";
+      context->fill();
     }
+    context->strokeStyle = "#e80";
+    context->stroke();
     context->fillStyle = "#fff";
     context->font.size = 20;
+    context->textAlign = "center";
     context->fillText(label, width / 2, height / 2);    
     return canvas::OpenGLTexture::createTexture(context->getDefaultSurface());
   }
