5245b5e761e7611b74cd0306c60929239d10d2e6
Mikael Rekola <mikael.rekola@gmail.com>
2016-11-14 20:23:56 +0200

    create native surface if USE_NATIVE_SURFACE is defined

--- src/OpenGLView.cpp
+++ src/OpenGLView.cpp
@@ -13,7 +13,12 @@ using namespace std;
 void
 OpenGLView::initialize(FWPlatform * _platform) {
   FWViewBase::initialize(_platform);
-  sendMessage(Message(Message::CREATE_OPENGL_VIEW, getParentInternalId(), getInternalId()));
+#ifdef USE_NATIVE_SURFACE
+  Message::Type type = Message::CREATE_NATIVE_OPENGL_VIEW;
+#else
+  Message::Type type = Message::CREATE_OPENGL_VIEW;
+#endif
+  sendMessage(Message(type, getParentInternalId(), getInternalId()));
   logical_width = int(_platform->getDisplayWidth() / _platform->getDisplayScale());
   logical_height = int(_platform->getDisplayHeight() / _platform->getDisplayScale());
   actual_width = _platform->getDisplayWidth();
