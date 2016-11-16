75091bfd2394b33a259bc9ea9b48807f964a3af4
MSuni <mikko.suni@hotmail.com>
2016-11-16 13:37:30 +0200

    Fix AConfigurationEvent dup() return value

--- src/android/AndroidConfigurationEvent.h
+++ src/android/AndroidConfigurationEvent.h
@@ -10,7 +10,7 @@ public:
    : ConfigurationEvent(_timestamp), window(_window) { }
 
   ANativeWindow * getWindow() { return window; }
-  std::shared_ptr<EventBase> dup() const { return NULL; }
+  std::shared_ptr<EventBase> dup() const { return  std::make_shared<AndroidConfigurationEvent>(*this); }
   void dispatch(Element & element);
   
  private:
