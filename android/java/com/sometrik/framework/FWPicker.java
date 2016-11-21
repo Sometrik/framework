3dc513a64e1e2737b406cacd391c3ccb9676c1f2
MSuni <mikko.suni@hotmail.com>
2016-11-21 11:31:47 +0200

    Add adapter option number sorting

--- android/java/com/sometrik/framework/FWPicker.java
+++ android/java/com/sometrik/framework/FWPicker.java
@@ -1,5 +1,8 @@
 package com.sometrik.framework;
 
+import java.util.ArrayList;
+import java.util.TreeMap;
+
 import android.content.Context;
 import android.widget.ArrayAdapter;
 import android.widget.Spinner;
@@ -7,20 +10,30 @@ import android.widget.Spinner;
 public class FWPicker extends Spinner implements NativeMessageHandler {
   
   ArrayAdapter<String> adapter;
+  ArrayList<Integer> numberList;
+  TreeMap<Integer, String> valueMap;
 
   public FWPicker(Context context) {
     super(context);
-  adapter = new ArrayAdapter<String>(context, android.R.layout.simple_spinner_item);
+    adapter = new ArrayAdapter<String>(context, android.R.layout.simple_spinner_item);
+    valueMap = new TreeMap<Integer, String>();
+    numberList = new ArrayList<Integer>();
   }
 
   @Override
   public void handleMessage(NativeMessage message) {
     
-
-    switch(message.getMessage()){
+    switch (message.getMessage()) {
     case ADD_OPTION:
-      adapter.add(message.getTextValue());
+      valueMap.put(message.getValue(), message.getTextValue());
+      numberList.add(message.getValue());
+      for (int i = 0; i < numberList.size(); i++) {
+	adapter.add(valueMap.get(numberList.get(i)));
+      }
       setAdapter(adapter);
+      break;
+    default:
+      System.out.println("Message couldn't be handled by Picker");
       break;
     }
   }
