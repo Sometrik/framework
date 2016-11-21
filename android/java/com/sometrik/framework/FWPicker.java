05497eb3970d943008d265a498aafa24e311014b
MSuni <mikko.suni@hotmail.com>
2016-11-21 11:35:22 +0200

    Add onSelectedListener and native function call to picker

--- android/java/com/sometrik/framework/FWPicker.java
+++ android/java/com/sometrik/framework/FWPicker.java
@@ -4,20 +4,36 @@ import java.util.ArrayList;
 import java.util.TreeMap;
 
 import android.content.Context;
+import android.view.View;
+import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.Spinner;
 
 public class FWPicker extends Spinner implements NativeMessageHandler {
   
-  ArrayAdapter<String> adapter;
-  ArrayList<Integer> numberList;
-  TreeMap<Integer, String> valueMap;
+  private ArrayAdapter<String> adapter;
+  private ArrayList<Integer> numberList;
+  private TreeMap<Integer, String> valueMap;
+  
+  private native void pickerOptionSelected(int id);
 
   public FWPicker(Context context) {
     super(context);
     adapter = new ArrayAdapter<String>(context, android.R.layout.simple_spinner_item);
     valueMap = new TreeMap<Integer, String>();
     numberList = new ArrayList<Integer>();
+    
+    setOnItemSelectedListener(new OnItemSelectedListener() {
+
+      @Override
+      public void onItemSelected(AdapterView<?> arg0, View arg1, int position, long arg3) {
+	pickerOptionSelected(position);
+      }
+
+      @Override
+      public void onNothingSelected(AdapterView<?> arg0) {
+      }
+    });
   }
 
   @Override
