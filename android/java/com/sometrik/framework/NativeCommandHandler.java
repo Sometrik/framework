51baabeec605b97a4ef23f9d5c3d5d4c95d3f52e
MSuni <mikko.suni@hotmail.com>
2016-11-22 13:01:44 +0200

    Remove handleCommand from interface

--- android/java/com/sometrik/framework/NativeCommandHandler.java
+++ android/java/com/sometrik/framework/NativeCommandHandler.java
@@ -3,7 +3,6 @@ package com.sometrik.framework;
 import android.view.View;
 
 public interface NativeCommandHandler {
-  public void handleCommand(NativeCommand command);
   public void addChild(View view);
   public void showView();
   public void addOption(int position, String text);
