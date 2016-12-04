79b63bde9c6a7198fab23ccd3552289c8dbb8f44
Mikael Rekola <mikael.rekola@gmail.com>
2016-12-04 22:29:15 +0200

    add END_MODAL type and parameter

--- include/SysEvent.h
+++ include/SysEvent.h
@@ -3,6 +3,8 @@
 
 #include <EventBase.h>
 
+#include <string>
+
 class SysEvent : public EventBase {
 public:
   enum Type {
@@ -12,17 +14,25 @@ public:
     RESUME,
     PAUSE,
     STOP,
-    RESTART
+    RESTART,
+    END_MODAL
   };
  SysEvent(double _timestamp, Type _type) : EventBase(_timestamp), type(_type) { }
 
   std::shared_ptr<EventBase> dup() const override { return std::make_shared<SysEvent>(*this); }
   void dispatch(Element & element) override;
+
+  void setValue(int _value) { value = _value; }
+  void setTextValue(const std::string & _text_value) { text_value = _text_value; }
   
   Type getType() { return type; }
+  int getValue() { return value; }
+  const std::string & getTextValue() const { return text_value; }
 
  private:
   Type type;
+  int value = 0;
+  std::string text_value;
 };
 
 #endif
