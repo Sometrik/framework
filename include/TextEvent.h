b9bda444ebf0be9f979eb6143fe2775186009d06
Mikael Rekola <mikael.rekola@gmail.com>
2016-12-08 23:09:17 +0200

    remove element_id

--- include/TextEvent.h
+++ include/TextEvent.h
@@ -6,17 +6,15 @@
 
 class TextEvent : public EventBase {
 public:
-  TextEvent(double _timestamp, std::string _text, int _element_id = 0)
-    : EventBase(_timestamp), element_id(_element_id), text(_text) { }
+  TextEvent(double _timestamp, std::string _text)
+    : EventBase(_timestamp), text(_text) { }
 
   std::shared_ptr<EventBase> dup() const override { return std::make_shared<TextEvent>(*this); }
   void dispatch(Element & element) override;
 
-  int getElementId() const { return element_id; }
   const std::string & getText() const { return text; }
 
  private:
-  int element_id;
   std::string text;
 };
 
