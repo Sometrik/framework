1598386bcb1a057918fc0ff867e2df700b70073f
Mikael Rekola <mikael.rekola@gmail.com>
2016-12-09 19:38:56 +0200

    store EventHandler

--- src/Event.cpp
+++ src/Event.cpp
@@ -2,8 +2,11 @@
 #include <EventHandler.h>
 
 void
-Event::dispatch(EventHandler & element) {
+Event::dispatch(EventHandler & evh) {
   if (!isHandled()) {
     element.onEvent(*this);
+    if (isHandled() && !handler) {
+      handler = &evh;
+    }
   }
 }
