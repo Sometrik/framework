c73057a12e4a5b82da8ef8cb581d3704ad585dc5
Mikael Rekola <mikael.rekola@gmail.com>
2016-12-13 06:23:22 +0200

    make pop() blocking, add method recvOne()

--- include/EventQueue.h
+++ include/EventQueue.h
@@ -12,28 +12,29 @@ class EventQueue {
  public:
   EventQueue() {
     pthread_mutex_init(&_mutex, NULL);
+    pthread_cond_init(&_condv, NULL);
   }
   ~EventQueue() {
     pthread_mutex_destroy(&_mutex);
+    pthread_cond_destroy(&_condv);
   }
 
   void push(int internal_id, const Event & ev) {
     std::shared_ptr<Event> ptr(ev.dup());
     pthread_mutex_lock (&_mutex);
     data.push_front(std::pair<int, std::shared_ptr<Event> >(internal_id, ptr));
+    pthread_cond_signal(&_condv);
     pthread_mutex_unlock(&_mutex);
   }
   std::pair<int, std::shared_ptr<Event> > pop() {
     pthread_mutex_lock (&_mutex);
-    if (!data.empty()) {
-      auto ev = data.back();
-      data.pop_back();
-      pthread_mutex_unlock(&_mutex);
-      return ev;
-    } else {
-      pthread_mutex_unlock(&_mutex);
-      return std::pair<int, std::shared_ptr<Event> >(0, std::shared_ptr<Event>(0));
+    while (data.empty()) {
+      pthread_cond_wait(&_condv, &_mutex);
     }
+    auto ev = data.back();
+    data.pop_back();
+    pthread_mutex_unlock(&_mutex);
+    return ev;
   }
   bool empty() const {
     pthread_mutex_lock(&_mutex);
@@ -60,9 +61,15 @@ class EventQueue {
     }
     return events.size();
   }
+
+  void recvOne(EventHandler & evh) {
+    auto ed = pop();
+    ed.second->dispatch(evh);
+  }
   
  private:
   mutable pthread_mutex_t _mutex;
+  pthread_cond_t _condv;
   std::list<std::pair<int, std::shared_ptr<Event> > > data;
 };
 
