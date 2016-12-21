8131b17e0ef2958f66acc0f41b16d93a89981894
MSuni <mikko.suni@hotmail.com>
2016-12-21 13:55:03 +0200

    Add addRow function

--- android/java/com/sometrik/framework/FWTable.java
+++ android/java/com/sometrik/framework/FWTable.java
@@ -3,17 +3,27 @@ package com.sometrik.framework;
 import android.content.Context;
 import android.view.View;
 import android.widget.TableLayout;
+import android.widget.TableRow;
 
 public class FWTable extends TableLayout implements NativeCommandHandler {
   
+  Context context;
+  
   public FWTable(Context context) {
     super(context);
+    this.context = context;
+  }
+  
+  private void addRow(){
+    TableRow row = new TableRow(context);
+    //TODO
   }
 
 
 
 @Override
 public void addChild(View view) {
+  addRow();
   System.out.println("FWTable couldn't handle addChild command");
 }
 
