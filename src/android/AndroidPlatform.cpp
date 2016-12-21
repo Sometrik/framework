8bfd7e755e4115dd99142a93d63deb8b373cdd1b
MSuni <mikko.suni@hotmail.com>
2016-12-21 16:52:07 +0200

    Fix duplicate textValue1

--- src/android/AndroidPlatform.cpp
+++ src/android/AndroidPlatform.cpp
@@ -75,7 +75,7 @@ AndroidPlatform::sendCommand(const Command & command) {
   auto textValue2 = command.getTextValue2();
   jbyteArray jtextValue = 0, jtextValue2 = 0;
   if (!textValue.empty()) jtextValue = convertToByteArray(textValue);
-  if (!textValue2.empty()) jtextValue2 = convertToByteArray(textValue);
+  if (!textValue2.empty()) jtextValue2 = convertToByteArray(textValue2);
   
   jobject jcommand = env->NewObject(javaCache.nativeCommandClass, javaCache.nativeCommandConstructor, framework, commandTypeId, command.getInternalId(), command.getChildInternalId(), command.getValue(), jtextValue, jtextValue2, command.getFlags());
   env->CallStaticVoidMethod(javaCache.frameworkClass, javaCache.sendCommandMethod, framework, jcommand);
