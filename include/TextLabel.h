95532e1f40ad55fa34d91e4b45ea9d6b8f11b193
Mikael Rekola <mikael.rekola@gmail.com>
2016-12-21 16:36:51 +0200

    add hyperlink

--- include/TextLabel.h
+++ include/TextLabel.h
@@ -6,18 +6,21 @@
 
 class TextLabel : public Element {
  public:
- TextLabel(const std::string & _text, unsigned int _flags = 0) : Element(0, _flags), text(_text) { }
+  TextLabel(const std::string & _text, unsigned int _flags = 0) : Element(0, _flags), text(_text) { }
+ TextLabel(const std::string & _text, const std::string & _url) : Element(0, 1), text(_text), url(_url) { }
 
   void initialize(FWPlatform * _platform) override {
     Element::initialize(_platform);
     Command c(Command::CREATE_TEXTLABEL, getParentInternalId(), getInternalId());
     c.setTextValue(text);
+    c.setTextValue2(url);
     c.setFlags(getFlags());
     sendCommand(c);
   }
 
  private:
   std::string text;
+  std::string url;
 };
 
 #endif
