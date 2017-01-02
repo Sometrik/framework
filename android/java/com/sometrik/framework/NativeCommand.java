0674a0d898304b2a673f94a57baee865441c6fb7
MSuni <mikko.suni@hotmail.com>
2017-01-02 15:45:17 +0200

    Correct a call after refactor

--- android/java/com/sometrik/framework/NativeCommand.java
+++ android/java/com/sometrik/framework/NativeCommand.java
@@ -445,7 +445,7 @@ public class NativeCommand {
   	List <Purchase> purchaseList = inventory.getAllPurchases();
   	System.out.println("getting purchase history. Purchase list size: " + purchaseList.size());
   	for (Purchase purchase : inventory.getAllPurchases()){
-  		FrameWork.sendPurchaseHistory(purchase.getOrderId(), purchase.getSku(), purchase.getPurchaseTime() / 1000.0);
+  		FrameWork.sendPurchaseHistory(purchase.getPurchaseTime() / 1000.0, purchase.getSku(), purchase.getOrderId());
   	}
   }
   
