db926966a25847390cfabf480784117d688c8b86
MSuni <mikko.suni@hotmail.com>
2017-01-03 11:38:52 +0200

    Add isNew bool

--- include/PurchaseEvent.h
+++ include/PurchaseEvent.h
@@ -8,16 +8,18 @@ public:
   enum Type {
     PURCHASE_STATUS
   };
- PurchaseEvent(double _timestamp, Type _type) : Event(_timestamp), type(_type) { }
+ PurchaseEvent(double _timestamp, Type _type, bool _newPurchase) : Event(_timestamp), type(_type), newPurchase(_newPurchase) { }
 
   Event * dup() const override { return new PurchaseEvent(*this); }
   void dispatch(EventHandler & element) override;
   bool isBroadcast() const override { return true; }
   
   Type getType() { return type; }
+  bool isNew() { return newPurchase; }
 
  private:
   Type type;
+  bool newPurchase;
 };
 
 #endif
