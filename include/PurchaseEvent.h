1088696cd49759a9f303bf9e9986b04cc13fb206
MSuni <mikko.suni@hotmail.com>
2017-01-03 11:42:15 +0200

    Add productId to purchaseEvent

--- include/PurchaseEvent.h
+++ include/PurchaseEvent.h
@@ -8,7 +8,7 @@ public:
   enum Type {
     PURCHASE_STATUS
   };
- PurchaseEvent(double _timestamp, Type _type, bool _newPurchase) : Event(_timestamp), type(_type), newPurchase(_newPurchase) { }
+ PurchaseEvent(double _timestamp, int _productId, Type _type, bool _newPurchase) : Event(_timestamp), type(_type), newPurchase(_newPurchase), productId(_productId) { }
 
   Event * dup() const override { return new PurchaseEvent(*this); }
   void dispatch(EventHandler & element) override;
@@ -16,10 +16,12 @@ public:
   
   Type getType() { return type; }
   bool isNew() { return newPurchase; }
+  int getProductId() { return productId; }
 
  private:
   Type type;
   bool newPurchase;
+  int productId;
 };
 
 #endif
