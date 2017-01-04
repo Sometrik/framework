8d294417e83e93e573fe648a798b0c545c4cbc64
Mikael Rekola <mikael.rekola@gmail.com>
2017-01-04 20:14:23 +0200

    add IAP public key

--- include/FWApplication.h
+++ include/FWApplication.h
@@ -9,11 +9,16 @@ class FWApplication : public Element {
 public:
   FWApplication(const char * _name, bool _full_screen = false)
     : name(_name), full_screen(_full_screen) { }
-    
+
+ FWApplication(const char * _name, const char * _iap_public_key, bool _full_screen = false)
+   : name(_name), iap_public_key(iap_public_key), full_screen(_full_screen) { }
+
   void initialize(FWPlatform * _platform) override {
     Element::initialize(_platform);
     Command c(Command::CREATE_APPLICATION, getParentInternalId(), getInternalId());
+    c.setFlags(iap_public_key.empty() ? 0 : 128);
     c.setTextValue(name);
+    c.setTextValue2(iap_public_key);
     sendCommand(c);
   }
 
@@ -39,7 +44,7 @@ public:
   }
 
  private:
-  std::string name;
+  std::string name, iap_public_key;
   bool full_screen;
   std::shared_ptr<Logger> logger;
 };
