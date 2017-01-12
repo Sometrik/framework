480c1ddd0a5969b0a356211943bfc2a2ac5e1164
Mikael Rekola <mikael.rekola@gmail.com>
2017-01-12 20:31:12 +0200

    add TableLayout.h

---
+++ include/TableLayout.h
@@ -0,0 +1,17 @@
+#ifndef _TABLELAYOUT_H_
+#define _TABLELAYOUT_H_
+
+#include <Element.h>
+#include <Command.h>
+
+class TableLayout : public Element {
+ public:
+  TableLayout(int _direction) : direction(_direction) { }
+
+  void initialize(FWPlatform * _platform) override {
+    Element::initialize(_platform);
+    sendCommand(Command(Command::CREATE_TABLE_LAYOUT, getParentInternalId(), getInternalId()));
+  }
+};
+
+#endif
