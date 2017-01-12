84f654d7121b59798d5622131b5d7558b62e0d26
Mikael Rekola <mikael.rekola@gmail.com>
2017-01-13 01:02:30 +0200

    remove setCaption(), add accessor for getName()

--- include/FWApplication.h
+++ include/FWApplication.h
@@ -22,12 +22,6 @@ public:
     sendCommand(c);
   }
 
-  void setCaption(const std::string & s) {
-    Command c(Command::SET_CAPTION, getId());
-    c.setTextValue(s);
-    sendCommand(c);
-  }
-
   void launchBrowser(const std::string & input_url) {
     sendCommand(Command(Command::LAUNCH_BROWSER, getInternalId(), input_url));
   }
@@ -42,6 +36,8 @@ public:
     }
     return *logger;
   }
+
+  const std::string & getName() const { return name; }
 
  private:
   std::string name, iap_public_key;
