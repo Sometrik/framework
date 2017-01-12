9cb62be721cdccce4cebaa1cfdda8657060739eb
Mikael Rekola <mikael.rekola@gmail.com>
2017-01-12 17:52:59 +0200

    add filename to ImageElemnt

--- include/ImageElement.h
+++ include/ImageElement.h
@@ -6,19 +6,18 @@
 
 class ImageElement : public Element {
  public:
-  ImageElement(int _width, int _height) : width(_width), height(_height) { }
+ ImageElement(const std::string & _filename) : filename(_filename) { }
 
   void initialize(FWPlatform * _platform) override {
     Element::initialize(_platform);
     Command c(Command::CREATE_IMAGE_ELEMENT, getParentInternalId(), getInternalId());
-    c.setWidth(width);
-    c.setHeight(height);
+    c.setTextValue(filename);
     c.setLayoutWeight(getLayoutWeight());
     sendCommand(c);
   }
 
  private:
-  int width, height;
+  std::string filename;
 };
 
 #endif
