081928a78143076d860e728d7c83e51bbf80eca9
Mikael Rekola <mikael.rekola@gmail.com>
2017-01-13 19:56:16 +0200

    add Property.h

---
+++ include/Property.h
@@ -0,0 +1,74 @@
+#ifndef _PROPERTY_H_
+#define _PROPERTY_H_
+
+#include <Notifier.h>
+
+class PropertyBase : public Notifier {
+ public:
+  PropertyBase() { }
+
+  void call() override { }
+
+  virtual std::string getStringValue() const = 0;
+  virtual bool getBoolValue() const = 0;
+
+  void set(bool t) { call(t); }
+  void set(int i) { call(i); }
+  void set(const std::string & s) { call(i); }
+};
+
+template<class T>
+class Property : public PropertyBase {
+ public:
+  Property() : data() { }
+  Property(const T & _data) : data(_data) { }
+  Property(const Property<T> & _other) : data(_other.data) { }
+
+  Property & operator=(const Property<T> & _other) {
+    data = _other.data;
+    notify(data);
+    return *this;
+  }
+
+  Property & operator=(const T & _other) {
+    data = T;
+    notify(data);
+    return *this;
+  }
+
+  T & get() { return data; }
+  const T & get() const { return data; }
+
+  void call(bool t) override { assignBoundVar(data, t); }
+  void call(int i) override { assignBoundVar(data, i); }
+  void call(const std::string & s) { assignBoundVar(data, s); }
+  
+  std::string getStringValue() const override {
+    std::string s;
+    assignBoundVar(s, data);
+    return s;
+  }
+
+  bool getBoolValue() const override {
+    bool b;
+    assignBoundVar(b, data);
+    return b;
+  }
+
+ private:
+  T data;
+};
+
+class NullProperty : public PropertyBase {
+ public:
+  NullProperty() { }
+
+  void call(bool t) override { }
+  void call(int i) override { }
+  void call(const std::string & s) { }
+
+  std::string getStringValue() const override { return ""; }
+  bool getBoolValue() const override { return false; }
+};
+
+#endif
