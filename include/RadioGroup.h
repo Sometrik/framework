0f18496f90677b795edeff73f25dd22f7248736e
Mikael Rekola <mikael.rekola@gmail.com>
2017-01-13 18:32:27 +0200

    add class RadioGroup

---
+++ include/RadioGroup.h
@@ -0,0 +1,60 @@
+#ifndef _RADIOGROUP_H_
+#define _RADIOGROUP_H_
+
+#include <Element.h>
+#include <Command.h>
+#include <ValueEvent.h>
+
+class RadioGroup : public Element {
+ public:
+  RadioGroup() { }
+  
+  void initialize(FWPlatform * _platform) override {
+    Element::initialize(_platform);
+    Command c(Command::CREATE_RADIO_GROUP, getParentInternalId(), getInternalId());
+    c.setLayoutWeight(getLayoutWeight());
+    sendCommand(c);
+    for (auto & o : options) {
+      initializeOption(o);
+    }
+  }
+
+  void addOption(int id, const std::string & name) {
+    if (options.empty()){
+      value = id;
+    }
+    options.push_back(std::pair<int, std::string>(id, name));
+    if (isInitialized()) {
+      initializeOption(options.back());
+    }
+  }
+
+  void onValueEvent(ValueEvent & ev) override {
+    value = ev.getValue();
+    notify(value);
+  }
+
+  void call() override { call(value); }
+  void call(bool t) override { call(t ? value : 0); }
+  void call(int i) override {
+    value = i;
+    Command c(Command::SET_INT_VALUE, getInternalId());
+    c.setValue(value);
+    sendCommand(c);
+  }
+  void call(const std::string & s) override { }
+
+ protected:
+  void initializeOption(const std::pair<int, std::string> & o) {
+    Command c(Command::ADD_OPTION, getInternalId(), 0);
+    c.setValue(o.first);
+    c.setTextValue(o.second);
+    sendCommand(c);
+  }
+
+ private:
+  int value = 0;
+  std::vector<std::pair<int, std::string> > options;
+};
+
+#endif
