42e52e46bd30fd05343e32b341bdd2a98dfd3f16
Mikael Rekola <mikael.rekola@gmail.com>
2017-01-13 14:13:19 +0200

    simplify conversion from bool to string

--- include/shims.h
+++ include/shims.h
@@ -6,7 +6,7 @@
 
 inline void assignBoundVar(std::string & a, const std::string & b) { a = b; }
 inline void assignBoundVar(int & a, const std::string & b) { a = atoi(b.c_str()); }
-inline void assignBoundVar(bool & a, const std::string & b) { a = atoi(b.c_str()) != 0; }
+inline void assignBoundVar(bool & a, const std::string & b) { a = !b.empty() && b != "0" && b != "false"; }
 inline void assignBoundVar(float & a, const std::string & b) { a = atof(b.c_str()) != 0; }
 
 inline void assignBoundVar(std::string & a, bool b) { a = std::to_string(b); }
