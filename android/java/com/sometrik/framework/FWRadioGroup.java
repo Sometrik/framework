05ccbb1ea7ae7a523c7bd18d586f6678bb74aed9
MSuni <mikko.suni@hotmail.com>
2017-01-16 15:34:42 +0200

    Add FWRadioGroup

---
+++ android/java/com/sometrik/framework/FWRadioGroup.java
@@ -0,0 +1,57 @@
+package com.sometrik.framework;
+
+import android.content.Context;
+import android.view.View;
+import android.widget.RadioGroup;
+
+public class FWRadioGroup extends RadioGroup implements NativeCommandHandler {
+  
+  private Context context;
+  
+  public FWRadioGroup(Context context){
+    super(context);
+    this.context = context;
+  }
+
+  @Override
+  public void addChild(View view) {
+    // TODO Auto-generated method stub
+    
+  }
+
+  @Override
+  public void removeChild(int id) {
+    // TODO Auto-generated method stub
+    
+  }
+
+  @Override
+  public void showView() {
+    // TODO Auto-generated method stub
+    
+  }
+
+  @Override
+  public void addOption(int optionId, String text) {
+    // TODO Auto-generated method stub
+    
+  }
+
+  @Override
+  public void setValue(String v) {
+    // TODO Auto-generated method stub
+    
+  }
+
+  @Override
+  public void setValue(int v) {
+    // TODO Auto-generated method stub
+    
+  }
+
+  @Override
+  public int getElementId() {
+    return getId();
+  }
+
+}
