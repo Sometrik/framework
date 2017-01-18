2e922320faa36eb3e584b7eb453a7767cf632741
MSuni <mikko.suni@hotmail.com>
2017-01-18 14:06:36 +0200

    Add FWScrollView

---
+++ android/java/com/sometrik/framework/FWScrollView.java
@@ -0,0 +1,63 @@
+package com.sometrik.framework;
+
+import android.content.Context;
+import android.view.View;
+import android.widget.ScrollView;
+
+public class FWScrollView extends ScrollView implements NativeCommandHandler {
+
+  Context context;
+  FrameWork frame;
+  
+  public FWScrollView(Context context) {
+    super(context);
+    this.context = context;
+    frame = (FrameWork)context;
+  }
+
+  @Override
+  public void addChild(View view) {
+    addChild(view);
+  }
+
+  @Override
+  public void removeChild(int id) {
+    View view = getChildAt(0);
+    if (view != null){
+      if (view.getId() == id){
+        this.removeChild(0);
+      }
+    }
+  }
+
+  @Override
+  public void showView() {
+    frame.setCurrentView(this);
+  }
+
+  @Override
+  public void addOption(int optionId, String text) {
+    System.out.println("FWScrollView couldn't handle command");
+  }
+
+  @Override
+  public void setValue(String v) {
+    System.out.println("FWScrollView couldn't handle command");
+  }
+
+  @Override
+  public void setValue(int v) {
+    System.out.println("FWScrollView couldn't handle command");
+  }
+
+  @Override
+  public void setEnabled(Boolean enabled) {
+    System.out.println("FWScrollView couldn't handle command");
+  }
+
+  @Override
+  public int getElementId() {
+    return getId();
+  }
+
+}
