6967727ca9fc5a84e37525448f10a19ff21a6a52
MSuni <mikko.suni@hotmail.com>
2017-01-18 13:28:16 +0200

    Add FWButton

---
+++ android/java/com/sometrik/framework/FWButton.java
@@ -0,0 +1,58 @@
+package com.sometrik.framework;
+
+import android.content.Context;
+import android.view.View;
+import android.widget.Button;
+
+public class FWButton extends Button implements NativeCommandHandler {
+
+  Context context;
+  
+  public FWButton(Context context) {
+    super(context);
+    this.context = context;
+    
+  }
+
+
+  @Override
+  public void addChild(View view) {
+    System.out.println("FWButton couldn't handle command");
+  }
+  
+  @Override
+  public void removeChild(int id) {
+    System.out.println("FWButton couldn't handle command");
+  }
+
+  @Override
+  public void showView() {
+    System.out.println("FWButton couldn't handle command");
+  }
+
+  @Override
+  public void addOption(int optionId, String text) {
+    System.out.println("FWButton couldn't handle command");
+  }
+
+  @Override
+  public void setValue(String v) {
+    setText(v);
+  }
+  
+  @Override
+  public void setValue(int v) {
+    System.out.println("FWButton couldn't handle command");
+  }
+
+  @Override
+  public int getElementId() {
+    return getId();
+  }
+
+  @Override
+  public void setEnabled(Boolean enabled) {
+    System.out.println("FWButton couldn't handle command");
+  }
+
+}
