e742b1afe094a5e576761fae11e19efae55c555a
Mikael Rekola <mikael.rekola@gmail.com>
2017-01-20 04:30:26 +0200

    add Selection class

---
+++ include/Selection.h
@@ -0,0 +1,37 @@
+#ifndef _SELECTION_H_
+#define _SELECTION_H_
+
+#include <memory>
+#include <vector>
+
+class Element;
+
+class Selection {
+ public:
+  Selection() { }
+  Selection(const std::vector<std::shared_ptr<Element> > & _data)
+    : data(_data) { }
+  Selection(const std::shared_ptr<Element> & e) {
+    add(e);
+  }
+
+  Selection & show();
+  Selection & hide();
+  Selection & remove();
+  Selection & style(const std::string & key, const std::string & value);
+
+  Selection parent() const;
+  Selection parents() const;
+  Selection children() const;
+
+  void add(const std::shared_ptr<Element> & e) { data.push_back(e); }
+  void clear() { data.clear(); }
+    
+  bool empty() const { return data.empty(); }
+  size_t size() const { return data.size(); }
+  
+ private:
+  std::vector<std::shared_ptr<Element> > data;
+};
+
+#endif
---
+++ src/Selection.cpp
@@ -0,0 +1,56 @@
+#include <Selection.h>
+#include <Element.h>
+
+using namespace std;
+
+Selection &
+Selection::show() {
+  for (auto & e : data) {
+    e->show();
+  }
+  return *this;
+}
+
+Selection &
+Selection::hide() {
+  for (auto & e : data) {
+    e->hide();
+  }
+  return *this;
+}
+
+Selection &
+Selection::remove() {
+  // TODO: remove selected elements
+  data.clear();
+  return *this;
+}
+
+Selection &
+Selection::style(const std::string & key, const std::string & value) {
+  for (auto & e : data) {
+    e->style(key, value);
+  }
+  return *this;
+}
+
+Selection
+Selection::parent() const {
+  // TODO: return a selection of parents of current selection
+  return Selection();
+}
+
+Selection
+Selection::parents() const {
+  // TODO: return a selection of all parents of current selection
+  return Selection();
+}
+
+Selection
+Selection::children() const {
+  vector<shared_ptr<Element> > all_children;
+  for (auto & e : data) {
+    all_children.insert(all_children.end(), e->getChildren().begin(), e->getChildren().end());
+  }
+  return Selection(all_children);
+}
