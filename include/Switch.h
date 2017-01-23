b59ee5648b4d5c91c442e4b6b50ecab6b64d5f9f
Mikael Rekola <mikael.rekola@gmail.com>
2017-01-24 00:47:25 +0200

    fix typo, and rename fields

--- include/Switch.h
+++ include/Switch.h
@@ -8,8 +8,8 @@
 
 class Switch : public InputElement {
  public:
-  Switch(const std::string & _label) : label(_label) { }
-  Switch(const std::string & _label, const std::string & _label2) : label(_label), label2(_label2) { }
+  Switch(const std::string & _on_label, const std::string & _off_label)
+    : on_label(_on_label), off_label(_off_label) { }
 
   bool isA(const std::string & className) override {
     if (className == "Switch") return true;
@@ -27,15 +27,15 @@ class Switch : public InputElement {
   void initialize(FWPlatform * _platform) override {
     Element::initialize(_platform);
     Command c(Command::CREATE_SWITCH, getParentInternalId(), getInternalId());
-    c.setTextValue(label);
-    c.setTextValue(label2);
+    c.setTextValue(on_label);
+    c.setTextValue2(off_label);
     c.setLayoutWeight(getLayoutWeight());
     sendCommand(c);
   }
 
  private:
   bool value = false;
-  std::string label, label2;
+  std::string on_label, off_label;
 };
 
 #endif
