4022c9f57f62909f3fea8e10481bfd36520de888
MSuni <mikko.suni@hotmail.com>
2017-02-02 11:42:55 +0200

    Add autoSize cells
    this can be set with setAutoSize

--- android/java/com/sometrik/framework/FWTable.java
+++ android/java/com/sometrik/framework/FWTable.java
@@ -5,6 +5,7 @@ import java.util.ArrayList;
 import android.content.Context;
 import android.util.Log;
 import android.view.View;
+import android.view.ViewGroup;
 import android.view.ViewParent;
 import android.widget.TableLayout;
 import android.widget.TableRow;
@@ -16,27 +17,55 @@ public class FWTable extends TableLayout implements NativeCommandHandler {
   int columnCount = 4;
   ArrayList<TableRow> rowList;
   ArrayList<View> dataList;
+  ViewGroup.LayoutParams params;
+  boolean autoSize = false;
   
   public FWTable(FrameWork frameWork) {
     super(frameWork);
     frame = frameWork;
+    params = new ViewGroup.LayoutParams(ViewGroup.LayoutParams.WRAP_CONTENT, ViewGroup.LayoutParams.WRAP_CONTENT);
+    setLayoutParams(params);
     rowList = new ArrayList<TableRow>();
     TableRow firstRow = new TableRow(frame);
+    firstRow.setLayoutParams(params);
     rowList.add(firstRow);
     addView(firstRow);
     dataList = new ArrayList<View>();
   }
-  
-  private TableRow getCurrentRow(){
-    if ((dataList.size() + 1) / columnCount > rowList.size()) {
-      Log.d("table", "New Row");
-      TableRow row = new TableRow(frame);
-      rowList.add(row);
-      addView(row);
-      return row;
+
+  private TableRow getCurrentRow(View view) {
+    if (autoSize) {
+      // Check that row wouldn't go over the screen
+      TableRow row = rowList.get(rowList.size() - 1);
+      Log.d("table", "Row Size Check. Before: " + frame.measureViewWidth(row));
+      Log.d("table", "Row Size Check. New: " + frame.measureViewWidth(view));
+      Log.d("table", "Row Size Check. Screen Size: " + frame.getScreenWidth() / 2);
+      if (frame.measureViewWidth(row) + frame.measureViewWidth(view) > frame.getScreenWidth() / 2) {
+	Log.d("table", "Too big yo");
+	return createNewRow();
+      } else {
+	return rowList.get(rowList.size() - 1);
+      }
     } else {
-      return rowList.get(rowList.size() - 1);
+      if ((dataList.size() + 1) / columnCount > rowList.size()) {
+	return createNewRow();
+      } else {
+	return rowList.get(rowList.size() - 1);
+      }
     }
+  }
+  
+  private TableRow createNewRow() {
+    Log.d("table", "New Row");
+    TableRow row = new TableRow(frame);
+    row.setLayoutParams(params);
+    rowList.add(row);
+    addView(row);
+    return row;
+  }
+  
+  public void setAutoSize(boolean enabled){
+    autoSize = enabled;
   }
   
   public void setColumnCount(int columnCount) { this.columnCount = columnCount; };
@@ -45,8 +74,9 @@ public class FWTable extends TableLayout implements NativeCommandHandler {
   @Override
   public void addChild(View view) {
     dataList.add(view);
-    TableRow row = getCurrentRow();
+    TableRow row = getCurrentRow(view);
     row.addView(view);
+    Log.d("table", "Ellipsis Full Row: " + frame.measureViewWidth(row));
   }
   
   @Override
