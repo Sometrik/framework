65ebfdb4c71e0183db5c3ef1f7361ef6f61200b7
Mikael Rekola <mikael.rekola@gmail.com>
2017-02-06 19:39:37 +0200

    add class HeadingText

---
+++ include/HeadingText.h
@@ -0,0 +1,30 @@
+#ifndef _HEADINGTEXT_H_
+#define _HEADINGTEXT_H_
+
+#include <Element.h>
+#include <Command.h>
+
+class HeadingText : public Element {
+ public:
+  HeadingText(const std::string & _text, unsigned int _flags = 0) : Element(0, _flags), text(_text) { }
+
+  bool isA(const std::string & className) override {
+    if (className == "HeadingText") return true;
+    return Element::isA(className);
+  }
+
+ protected:
+  void initialize(FWPlatform * _platform) override {
+    Element::initialize(_platform);
+    Command c(Command::CREATE_HEADING_TEXT, getParentInternalId(), getInternalId());
+    c.setTextValue(text);
+    c.setFlags(getFlags());
+    sendCommand(c);
+  }
+
+ private:
+  std::string text;
+  std::string url;
+};
+
+#endif
