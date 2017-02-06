9eed93ba1eaa8dce8b6f099bf1b12187ecc76619
Mikael Rekola <mikael.rekola@gmail.com>
2017-02-06 21:33:06 +0200

    add constructor parameters

--- include/InputElement.h
+++ include/InputElement.h
@@ -5,7 +5,7 @@
 
 class InputElement : public Element {
  public:
-  InputElement() { }
+ InputElement(int _id = 0, unsigned int _flags = 0) : Element(_id, _flags) { }
 
   bool isA(const std::string & className) override {
     if (className == "InputElement") return true;
