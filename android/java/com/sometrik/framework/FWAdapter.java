c6ad26254261c9871b7c67faf33b0b5dd3d6f30d
MSuni <mikko.suni@hotmail.com>
2017-02-10 13:57:03 +0200

    Fix Adapter to be created dynamically

--- android/java/com/sometrik/framework/FWAdapter.java
+++ android/java/com/sometrik/framework/FWAdapter.java
@@ -11,6 +11,7 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
+import android.widget.LinearLayout;
 import android.widget.TextView;
 
 public class FWAdapter extends ArrayAdapter<View> {
@@ -18,25 +19,25 @@ public class FWAdapter extends ArrayAdapter<View> {
   private List<View> viewList;
   private ArrayList<AdapterData> dataList;
   private FrameWork frame;
+  private LinearLayout.LayoutParams listItemParams;
   
   public FWAdapter(Context context, List<View> viewList) {
     super(context, 0, viewList);
-    this.frame = (FrameWork)context;
-//    this.viewList = viewList;
-    this.dataList = new ArrayList<AdapterData>();
+    this.frame = (FrameWork) context;
+    dataList = new ArrayList<AdapterData>();
+    listItemParams = new LinearLayout.LayoutParams(0, LinearLayout.LayoutParams.WRAP_CONTENT);
+    listItemParams.weight = 1;
+    listItemParams.leftMargin = 2;
   }
 
   public ArrayList<String> getDataRow(int rowId) {
     if (dataList.size() >= rowId + 1) {
-      return dataList.get(rowId).dataList;
+      return dataList.get(rowId).stringList;
     }
     Log.d("adapter", "no row found");
     return null;
   }
   
-  public void addItem(View view){
-//    viewList.add(view);
-  }
   public void addItem(ArrayList<String> cellItems){
     dataList.add(new AdapterData(cellItems));
   }
@@ -58,46 +59,39 @@ public class FWAdapter extends ArrayAdapter<View> {
 
   @Override
   public View getView(int position, View convertView, ViewGroup parent) {
+
+    LinearLayout layout = new LinearLayout(frame);
+
+    AdapterData data = dataList.get(position);
     
-	
-	
-	LayoutInflater inflater = frame.getLayoutInflater();
-	convertView=inflater.inflate(R.layout.multicolumn_data, null);
-	
-	
-	TextView txtFirst=(TextView) convertView.findViewById(R.id.name);
-	TextView txtSecond=(TextView) convertView.findViewById(R.id.gender);
-	TextView txtThird=(TextView) convertView.findViewById(R.id.age);
-	TextView txtFourth=(TextView) convertView.findViewById(R.id.status);
-	
-	AdapterData data = dataList.get(position);
-	txtFirst.setText(data.getData(0));
-	txtSecond.setText(data.getData(1));
-	txtThird.setText(data.getData(2));
-	txtFourth.setText(data.getData(3));
-	
-//    DisplayMetrics displaymetrics = new DisplayMetrics();
-////    int dp = (int) TypedValue.applyDimension(TypedValue.COMPLEX_UNIT_DIP, 130, displaymetrics);
-//    AbsListView.LayoutParams params = new AbsListView.LayoutParams(ViewGroup.LayoutParams.WRAP_CONTENT, 130);
-//    View view = viewList.get(position);
-//    view.setLayoutParams(params);
-    return convertView;
+    for (int i = 0; i < data.getSize(); i++){
+      TextView txtFirst = new TextView(frame);
+      txtFirst.setLayoutParams(listItemParams);
+      layout.addView(txtFirst);
+      txtFirst.setText(data.getData(i));
+    }
+
+    return layout;
   }
   
   public class AdapterData {
 
-    private ArrayList<String> dataList;
+    private ArrayList<String> stringList;
 
-    public AdapterData(ArrayList<String> dataList) {
-      this.dataList = dataList;
+    public AdapterData(ArrayList<String> stringList) {
+      this.stringList = stringList;
+    }
+
+    public int getSize() {
+      return stringList.size();
     }
 
     public String getData(int position) {
 
-      if (position < dataList.size()) {
-	return dataList.get(position);
+      if (position < stringList.size()) {
+	return stringList.get(position);
       } else {
-	return dataList.get(0);
+	return stringList.get(0);
       }
     }
 
