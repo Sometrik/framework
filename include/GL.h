1f15ea1345491fef82d15ac0bf107a29e7532743
MSuni <mikko.suni@hotmail.com>
2017-03-22 15:22:08 +0200

    Clean up importgl

--- include/GL.h
+++ include/GL.h
@@ -32,4 +32,44 @@
 
 #endif
 
+#if defined __APPLE__ || defined __ANDROID__
+#ifndef GL_RED
+#define GL_RED 0
+#endif
+#ifndef GL_R8
+#define GL_R8 0
+#endif
+#ifndef GL_R32F
+#define GL_R32F 0
+#endif
+#ifndef GL_RG8
+#define GL_RG8 0
+#endif
+#ifndef GL_RG
+#define GL_RG 0
+#endif
+#ifndef GL_RGB8
+#define GL_RGB8 0
+#endif
+#ifndef GL_RGBA8
+#define GL_RGBA8 0
+#endif
+#ifndef GL_COMPRESSED_RGB8_ETC2
+#define GL_COMPRESSED_RGB8_ETC2 0
+#endif
+#ifndef GL_COMPRESSED_RGB_S3TC_DXT1_EXT
+#define GL_COMPRESSED_RGB_S3TC_DXT1_EXT 0
+#endif
+#ifndef GL_COMPRESSED_RGBA_S3TC_DXT5_EXT
+#define GL_COMPRESSED_RGBA_S3TC_DXT5_EXT 0
+#endif
+#ifndef GL_COMPRESSED_RED_RGTC1
+#define GL_COMPRESSED_RED_RGTC1 0x8DBB
+#endif
+#ifndef GL_COMPRESSED_RG_RGTC2
+#define GL_COMPRESSED_RG_RGTC2 0x8DBD
+#endif
+#endif
+
+
 #endif
--- src/android/importgl.c
+++ src/android/importgl.c
@@ -1,29 +1,8 @@
-#undef WIN32
-#undef LINUX
-#ifdef _MSC_VER
-// Desktop or mobile Win32 environment:
-#define WIN32
-#else
-// Linux environment:
 #define LINUX
-#endif
 
-#ifndef DISABLE_IMPORTGL
-
-#if defined(WIN32)
-#define WIN32_LEAN_AND_MEAN
-#include <windows.h>
-#include <tchar.h>
-static HMODULE sGLESDLL = NULL;
-#endif // WIN32
-
-#ifdef LINUX
 #include <stdlib.h>
 #include <dlfcn.h>
 static void *sGLESSO = NULL;
-#endif // LINUX
-
-#endif /* DISABLE_IMPORTGL */
 
 #define IMPORTGL_NO_FNPTR_DEFS
 #define IMPORTGL_API
@@ -31,48 +10,26 @@ static void *sGLESSO = NULL;
 #include "importgl.h"
 
 
-/* Imports function pointers to selected function calls in OpenGL ES Common
- * or Common Lite profile DLL or shared object. The function pointers are
- * stored as global symbols with equivalent function name but prefixed with
- * "funcPtr_". Standard gl/egl calls are redirected to the function pointers
- * with preprocessor macros (see importgl.h).
+/* Imports function pointers to selected function calls in OpenGL ES 2 or 3
+ * The function pointers are stored as global symbols with equivalent function
+ * name but prefixed with "funcPtr_". Standard gl/egl calls are redirected to
+ * the function pointers with preprocessor macros (see importgl.h).
  */
 int importGLInit()
 {
     int result = 1;
 
-#ifndef DISABLE_IMPORTGL
-
-#undef IMPORT_FUNC
-
-#ifdef WIN32
-    sGLESDLL = LoadLibrary(_T("lGLESv2"));
-    if (sGLESDLL == NULL)
-        sGLESDLL = LoadLibrary(_T("lGLESv2"));
-    if (sGLESDLL == NULL)
-        return 0;   // Cannot find OpenGL ES Common or Common Lite DLL.
-
-    /* The following fetches address to each egl & gl function call
-     * and stores it to the related function pointer. Casting through
-     * void * results in warnings with VC warning level 4, which
-     * could be fixed by casting to the true type for each fetch.
-     */
-#define IMPORT_FUNC(funcName) do { \
-        void *procAddress = (void *)GetProcAddress(sGLESDLL, _T(#funcName)); \
-        if (procAddress == NULL) result = 0; \
-        *((void **)&FNPTR(funcName)) = procAddress; } while (0)
-#endif // WIN32
-
-#ifdef LINUX
-#ifdef __ANDROID__
 //    sGLESSO = dlopen("libGLESv3_CM.so", RTLD_NOW);
-//    sGLESSO = dlopen("libGLESv3.so", RTLD_NOW);
-    sGLESSO = dlopen("libGLESv3", RTLD_NOW);
-#else /* !__ANDROID__ */
-    sGLESSO = dlopen("libGLES_CM.so", RTLD_NOW);
-    if (sGLESSO == NULL)
-        sGLESSO = dlopen("libGLES_CL.so", RTLD_NOW);
-#endif /* !__ANDROID__ */
+    sGLESSO = dlopen("libGLESv3.so", RTLD_NOW);
+    int es3 = 0;
+    if (sGLESSO == NULL) {
+      es3 = 1;
+      sGLESSO = dlopen("libGLESv2.so", RTLD_NOW);
+      if (sGLESSO == NULL) {
+        return 0;   // Cannot find OpenGL ES Common or Common Lite SO.
+      }
+    }
+
     if (sGLESSO == NULL)
         return 0;   // Cannot find OpenGL ES Common or Common Lite SO.
 
@@ -80,50 +37,13 @@ int importGLInit()
         void *procAddress = (void *)dlsym(sGLESSO, #funcName); \
         if (procAddress == NULL) result = 0; \
         *((void **)&FNPTR(funcName)) = procAddress; } while (0)
-#endif // LINUX
-
-#ifndef __ANDROID__
-  IMPORT_FUNC(eglChooseConfig);
-  IMPORT_FUNC(eglCreateContext);
-  IMPORT_FUNC(eglCreateWindowSurface);
-  IMPORT_FUNC(eglDestroyContext);
-  IMPORT_FUNC(eglDestroySurface);
-  IMPORT_FUNC(eglGetConfigAttrib);
-  IMPORT_FUNC(eglGetConfigs);
-  IMPORT_FUNC(eglGetDisplay);
-  IMPORT_FUNC(eglGetError);
-  IMPORT_FUNC(eglInitialize);
-  IMPORT_FUNC(eglMakeCurrent);
-  IMPORT_FUNC(eglSwapBuffers);
-  IMPORT_FUNC(eglTerminate);
-#endif /* !__ANDROID__ */
 
   IMPORT_FUNC(glBlendFunc);
   IMPORT_FUNC(glClear);
-//  IMPORT_FUNC(glClearColorx);
-  IMPORT_FUNC(glColor4x);
-  IMPORT_FUNC(glColorPointer);
   IMPORT_FUNC(glDisable);
-  IMPORT_FUNC(glDisableClientState);
   IMPORT_FUNC(glDrawArrays);
   IMPORT_FUNC(glEnable);
-  IMPORT_FUNC(glEnableClientState);
-  IMPORT_FUNC(glFrustumx);
   IMPORT_FUNC(glGetError);
-  IMPORT_FUNC(glLightxv);
-  IMPORT_FUNC(glLoadIdentity);
-  IMPORT_FUNC(glMaterialx);
-  IMPORT_FUNC(glMaterialxv);
-  IMPORT_FUNC(glMatrixMode);
-  IMPORT_FUNC(glMultMatrixx);
-  IMPORT_FUNC(glNormalPointer);
-  IMPORT_FUNC(glPopMatrix);
-  IMPORT_FUNC(glPushMatrix);
-  IMPORT_FUNC(glRotatex);
-  IMPORT_FUNC(glScalex);
-  IMPORT_FUNC(glShadeModel);
-  IMPORT_FUNC(glTranslatex);
-  IMPORT_FUNC(glVertexPointer);
   IMPORT_FUNC(glViewport);
   IMPORT_FUNC(glAttachShader);
   IMPORT_FUNC(glBindAttribLocation);
@@ -187,21 +107,11 @@ int importGLInit()
   IMPORT_FUNC(glVertexAttribDivisor);
   IMPORT_FUNC(glTexStorage2D);
 
-#endif /* DISABLE_IMPORTGL */
-
     return result;
 }
 
 
 void importGLDeinit()
 {
-#ifndef DISABLE_IMPORTGL
-#ifdef WIN32
-    FreeLibrary(sGLESDLL);
-#endif
-
-#ifdef LINUX
-    dlclose(sGLESSO);
-#endif
-#endif /* DISABLE_IMPORTGL */
+  dlclose(sGLESSO);
 }
