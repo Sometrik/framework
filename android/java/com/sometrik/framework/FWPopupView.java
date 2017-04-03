0b23eb30b0a1651c3135cec932c60678f91a1ddf
MSuni <mikko.suni@hotmail.com>
2017-04-03 11:28:26 +0300

    Add FWPopupView

---
+++ android/java/com/sometrik/framework/FWPopupView.java
@@ -0,0 +1,87 @@
+package com.sometrik.framework;
+
+import android.view.Gravity;
+import android.view.View;
+import android.widget.PopupWindow;
+
+public class FWPopupView extends PopupWindow implements NativeCommandHandler {
+
+  FrameWork frame;
+  View parentView;
+  int id;
+  
+  public FWPopupView(FrameWork frame, View parentView, int id){
+    this.frame = frame;
+    this.parentView = parentView;
+    this.id = id;
+  }
+
+  @Override
+  public void onScreenOrientationChange(boolean isLandscape) {
+    
+  }
+
+  @Override
+  public void addChild(View view) {
+    System.out.println("setting new contentView for popupView");
+    this.setContentView(view);
+  }
+
+  @Override
+  public void addOption(int optionId, String text) {
+    System.out.println("command couldn't be handled by popupView");
+  }
+
+  @Override
+  public void addData(int rowNumber, int columnNumber, String text) {
+    System.out.println("command couldn't be handled by popupView");
+  }
+
+  @Override
+  public void setValue(String v) {
+    System.out.println("command couldn't be handled by popupView");
+  }
+
+  @Override
+  public void setValue(int v) {
+    if (v == 1){
+      showAtLocation(parentView, Gravity.CENTER, 0, 0);
+    } else if (v <= 0){
+      dismiss();
+    }
+  }
+
+  @Override
+  public void setViewEnabled(Boolean enabled) {
+    if (enabled){
+      showAtLocation(parentView, Gravity.CENTER, 0, 0);
+    } else {
+      dismiss();
+    }
+  }
+
+  @Override
+    public void setViewVisibility(boolean visible) {
+    if (visible){
+      showAtLocation(parentView, Gravity.CENTER, 0, 0);
+    } else {
+      dismiss();
+    }
+  }
+
+  @Override
+  public void setStyle(String key, String value) {
+    System.out.println("command couldn't be handled by popupView");
+  }
+
+  @Override
+  public void setError(boolean hasError, String errorText) {
+    System.out.println("command couldn't be handled by popupView");
+  }
+
+  @Override
+  public int getElementId() {
+    return id;
+  }
+  
+}
