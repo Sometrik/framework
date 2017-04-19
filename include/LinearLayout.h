28633c5982e063d4b365d7e04a96912767ccee21
MSuni <mikko.suni@hotmail.com>
2017-04-19 11:26:21 +0300

    Add setGravity and additionalInitCommands

--- include/LinearLayout.h
+++ include/LinearLayout.h
@@ -11,6 +11,22 @@ class LinearLayout : public Element {
  public:
   LinearLayout(int _direction, int _id) : Element(_id), direction(_direction) { }
 
+  void
+  setGravity(const char * gravity){
+    Command c(Command::SET_STYLE, getInternalId());
+    c.setTextValue("gravity");
+    if (gravity == "bottom"){
+      c.setTextValue2("bottom");
+    } else if (gravity == "top"){
+      c.setTextValue2("top");
+    } else if (gravity == "left"){
+      c.setTextValue2("left");
+    } else if (gravity == "right"){
+      c.setTextValue2("right");
+    }
+    additionalInitCommands.push_back(c);
+  }
+
   bool isA(const std::string & className) override {
     if (className == "LinearLayout") return true;
     return Element::isA(className);
@@ -22,10 +38,16 @@ class LinearLayout : public Element {
     Command c(Command::CREATE_LINEAR_LAYOUT, getParentInternalId(), getInternalId());
     c.setValue(direction);
     sendCommand(c);
+
+    for (int i = 0; i < additionalInitCommands.size(); i++){
+      sendCommand(additionalInitCommands.at(i));
+    }
   }
 			       
  private:
   int direction = FW_VERTICAL;
+  std::vector<Command> additionalInitCommands;
+
 };
 
 #endif
