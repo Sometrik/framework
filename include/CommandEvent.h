eb0b80f92ef2b51166a151eefa1058771512cc29
Mikael Rekola <mikael.rekola@gmail.com>
2017-05-20 19:19:06 +0300

    add secondary value

--- include/CommandEvent.h
+++ include/CommandEvent.h
@@ -7,8 +7,8 @@
 
 class CommandEvent : public Event {
 public:
-  CommandEvent(double _timestamp, int _element_id = 0, int _value = 0)
-    : Event(_timestamp), element_id(_element_id), value(_value) { }
+ CommandEvent(double _timestamp, int _element_id = 0, int _value = 0, int _value2 = 0)
+   : Event(_timestamp), element_id(_element_id), value(_value), value2(_value2) { }
 
   Event * dup() const override { return new CommandEvent(*this); }
   void dispatch(EventHandler & element) override {
@@ -20,12 +20,13 @@ public:
     }
     Event::dispatch(element);
   }
-
+  
   int getElementId() const { return element_id; }
   int getValue() const { return value; }
+  int getValue2() const { return value2; }
   
  private:
-  int element_id, value;
+  int element_id, value, value2;
   int edge = +1;
 };
 
