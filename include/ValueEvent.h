c3fccbe83f7a0c26339add4938fc676da7c5b4c3
Mikael Rekola <mikael.rekola@gmail.com>
2017-05-20 19:19:44 +0300

    add secondary value

--- include/ValueEvent.h
+++ include/ValueEvent.h
@@ -10,9 +10,9 @@
 class ValueEvent : public Event {
 public:
   ValueEvent(double _timestamp, const std::string & _text_value)
-    : Event(_timestamp), text(_text_value), value(0) { }
-  ValueEvent(double _timestamp, int _value)
-    : Event(_timestamp), value(_value) { }
+    : Event(_timestamp), text(_text_value), value(0), value2(0) { }
+  ValueEvent(double _timestamp, int _value, int _value2 = 0)
+    : Event(_timestamp), value(_value), value2(_value2) { }
 
   Event * dup() const override { return new ValueEvent(*this); }
   void dispatch(EventHandler & element) override {
@@ -27,10 +27,11 @@ public:
 
   const std::string & getTextValue() const { return text; }
   int getValue() const { return value; }
+  int getValue2() const { return value2; }
 
  private:
   std::string text;
-  int value;
+  int value, value2;
 };
 
 #endif
