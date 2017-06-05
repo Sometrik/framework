fed3f5d796b98496567561417426403002b746af
MSuni <mikko.suni@hotmail.com>
2017-06-05 13:27:54 +0300

    Change default text size

--- android/java/com/sometrik/framework/FWAdapter.java
+++ android/java/com/sometrik/framework/FWAdapter.java
@@ -344,7 +344,7 @@ public class FWAdapter extends ArrayAdapter<View> implements ExpandableListAdapt
 	TextView txtFirst = new TextView(frame);
 	txtFirst.setLayoutParams(listItemParams);
 	txtFirst.setTypeface(null, Typeface.BOLD);
-	txtFirst.setTextAppearance(frame, android.R.style.TextAppearance_DeviceDefault_Large);
+	txtFirst.setTextAppearance(frame, android.R.style.TextAppearance_DeviceDefault_Small);
 	txtFirst.setFocusable(false);
 	txtFirst.setFocusableInTouchMode(false);
 	txtFirst.setClickable(false);
@@ -377,7 +377,7 @@ public class FWAdapter extends ArrayAdapter<View> implements ExpandableListAdapt
 	txtFirst.setFocusable(false);
 	txtFirst.setFocusableInTouchMode(false);
 	txtFirst.setClickable(false);
-	txtFirst.setTextAppearance(frame, android.R.style.TextAppearance_DeviceDefault_Large);
+	txtFirst.setTextAppearance(frame, android.R.style.TextAppearance_DeviceDefault_Small);
 	txtFirst.setLayoutParams(listItemParams);
 	layout.addView(txtFirst);
 	txtFirst.setText(data.getData(i));
