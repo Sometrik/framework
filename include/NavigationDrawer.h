edef18188ad27215b44d8469cb4ecf62509f43aa
MSuni <mikko.suni@hotmail.com>
2017-06-06 13:34:21 +0300

    Add NavigationDrawer.h

---
+++ include/NavigationDrawer.h
@@ -0,0 +1,25 @@
+#ifndef _NAVIGATIONDRAWER_H_
+#define _NAVIGATIONDRAWER_H_
+
+#include <Element.h>
+#include <Command.h>
+
+class NavigationDrawer : public Element {
+ public:
+  NavigationDrawer(int _id = 0) : Element(_id) { }
+
+  bool isA(const std::string & className) const override {
+    if (className == "NavigationDrawer") return true;
+    return Element::isA(className);
+  }
+
+ protected:
+  void create() override {
+    Command c(Command::CREATE_NAVIGATIONVIEW, getParentInternalId(), getInternalId());
+    sendCommand(c);
+  }
+			       
+ private:
+};
+
+#endif
