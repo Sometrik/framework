9d2c6f186260eae990385bf4ae7449cf65624f79
MSuni <mikko.suni@hotmail.com>
2017-06-08 10:54:19 +0300

    Add setStyle options to FWDialog

--- android/java/com/sometrik/framework/FWDialog.java
+++ android/java/com/sometrik/framework/FWDialog.java
@@ -1,8 +1,11 @@
 package com.sometrik.framework;
 
 import android.app.Dialog;
+import android.view.Gravity;
 import android.view.View;
 import android.view.ViewGroup;
+import android.view.ViewGroup.LayoutParams;
+import android.widget.LinearLayout;
 import android.widget.ScrollView;
 
 public class FWDialog extends Dialog implements NativeCommandHandler{
@@ -18,10 +21,17 @@ public class FWDialog extends Dialog implements NativeCommandHandler{
     this.id = id;
     scrollView = new ScrollView(frame);
     setContentView(scrollView);
+    
+
+    LayoutParams params = getWindow().getAttributes();
+    params.height = LayoutParams.WRAP_CONTENT;
+    params.width = LayoutParams.MATCH_PARENT;
+    getWindow().setAttributes((android.view.WindowManager.LayoutParams) params);
   }
 
   @Override
   public void onScreenOrientationChange(boolean isLandscape) {
+    
   }
 
   @Override
@@ -37,6 +47,12 @@ public class FWDialog extends Dialog implements NativeCommandHandler{
   @Override
   public void addOption(int optionId, String text) {
     System.out.println("FWDialog couldn't handle the command");
+    switch (optionId){
+    case 0:
+      break;
+    case 1:
+      break;
+    }
   }
 
   @Override
@@ -78,7 +94,23 @@ public class FWDialog extends Dialog implements NativeCommandHandler{
 
   @Override
   public void setStyle(String key, String value) {
-    System.out.println("FWDialog couldn't handle the command");
+    if (key.equals("width")) {
+      ViewGroup.LayoutParams params = getWindow().getAttributes();
+      if (value.equals("wrap_content")) {
+	params.width = LinearLayout.LayoutParams.WRAP_CONTENT;
+      } else if (value.equals("match_parent")) {
+	params.width = LinearLayout.LayoutParams.MATCH_PARENT;
+      }
+      getWindow().setAttributes((android.view.WindowManager.LayoutParams) params);
+    } else if (key.equals("height")) {
+      ViewGroup.LayoutParams params = getWindow().getAttributes();
+      if (value.equals("wrap_content")) {
+	params.height = LinearLayout.LayoutParams.WRAP_CONTENT;
+      } else if (value.equals("match_parent")) {
+	params.height = LinearLayout.LayoutParams.MATCH_PARENT;
+      }
+      getWindow().setAttributes((android.view.WindowManager.LayoutParams) params);
+    }
   }
 
   @Override
