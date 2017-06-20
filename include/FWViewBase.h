c66f4dce7effb3e2f06f044def7d222b3edca9f9
Mikael Rekola <mikael.rekola@gmail.com>
2017-06-20 19:40:25 +0300

    rename label to title and set title on view switch

--- include/FWViewBase.h
+++ include/FWViewBase.h
@@ -7,8 +7,8 @@
 class FWViewBase : public Element {
  public:
   FWViewBase(int _id) : Element(_id) { }
-  FWViewBase(const std::string & _label, int _id)
-    : Element(_id), label(_label) { }
+  FWViewBase(const std::string & _title, int _id)
+    : Element(_id), title(_title) { }
 
   bool isA(const std::string & className) const override {
     if (className == "FWViewBase") return true;
@@ -19,13 +19,14 @@ class FWViewBase : public Element {
     Element::show();
     Command c(Command::SET_INT_VALUE, getInternalId());
     c.setValue(1);
+    c.setTextValue(getTitle());
     sendCommand(c);
   }
 
-  const std::string & getLabel() const { return label; }
-  
+  virtual std::string getTitle() const { return title; }
+    
  private:
-  std::string label;
+  std::string title;
 };
 
 #endif
