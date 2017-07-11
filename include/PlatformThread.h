d7229304c56a6e940896a9a7c8d6ac2b7811f2eb
Mikael Rekola <mikael.rekola@gmail.com>
2017-07-12 01:45:09 +0300

    add return value to start(), remove methods disconnect() and alert()

--- include/PlatformThread.h
+++ include/PlatformThread.h
@@ -13,15 +13,16 @@ class PlatformThread {
    : platform(_platform), runnable(_runnable) {
     
   }
+  PlatformThread(const PlatformThread & other) = delete;
+  PlatformThread & operator=(const PlatformThread & other) = delete;
+  
   virtual ~PlatformThread() { }  
   
-  virtual void start() = 0;
+  virtual bool start() = 0;
   virtual bool testDestroy() = 0;
   virtual void sendEventFromThread(const Event & ev) = 0;
   virtual void terminate() = 0;
-  virtual void disconnect() = 0;
   virtual void sleep(float t) = 0;
-  virtual void alert(const char * message) { }
   
   Runnable & getRunnable() { return *runnable; }
   Runnable * getRunnablePtr() { return runnable.get(); }
