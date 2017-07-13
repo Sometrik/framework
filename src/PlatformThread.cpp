f0fda12c59221f4260394ee19503de7cbe8b8309
Mikael Rekola <mikael.rekola@gmail.com>
2017-07-13 04:15:57 +0300

    add catch for any exception

--- src/PlatformThread.cpp
+++ src/PlatformThread.cpp
@@ -3,6 +3,9 @@
 #include <FWPlatform.h>
 #include <Runnable.h>
 #include <SysEvent.h>
+#include <Logger.h>
+
+#include <exception>
 
 using namespace std;
 
@@ -16,7 +19,11 @@ PlatformThread::sendEventFromThread(const Event & ev) {
 void
 PlatformThread::start2() {
   initialize();
-  getRunnable().start(this);
+  try {
+    getRunnable().start(this);
+  } catch (exception & e) {
+    getPlatform().getLogger().println("PlatformThread: Runnable threw an exception: " + string(e.what()));
+  }
   deinitialize();
 
   SysEvent ev(getPlatform().getTime(), SysEvent::THREAD_TERMINATED);
