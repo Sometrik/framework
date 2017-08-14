79bbf3dde2ac2db0853c23125fa0ea4fb1cdbda8
MSuni <mikko.suni@hotmail.com>
2017-08-14 14:08:48 +0300

    Add ChildClickListeners

--- android/java/com/sometrik/framework/FWLayout.java
+++ android/java/com/sometrik/framework/FWLayout.java
@@ -4,12 +4,16 @@ import android.content.Context;
 import android.view.Gravity;
 import android.view.View;
 import android.view.ViewParent;
+import android.view.View.OnClickListener;
+import android.widget.AdapterView;
 import android.widget.LinearLayout;
 import android.widget.LinearLayout.LayoutParams;
 
 public class FWLayout extends LinearLayout implements NativeCommandHandler {
   
   private FrameWork frame;
+  private boolean childListeners = false;
+  private ChildClickListener hostListener;
   
   public FWLayout(FrameWork frameWork) {
     super(frameWork);
@@ -23,9 +27,40 @@ public class FWLayout extends LinearLayout implements NativeCommandHandler {
  
   @Override
   public void addChild(View view) {
+    System.out.println("addChild on FWLayout " + view.getId());
     addView(view);
+    
+    if (childListeners) {
+      if (view instanceof AdapterView) {
+	return;
+      } else {
+	System.out.println("set listener to " + view.getId());
+	view.setOnClickListener(new OnClickListener() {
+	  @Override
+	  public void onClick(View v) {
+  	    System.out.println("child click " + v.getId());
+  	    int index = getChildIndex(v.getId());
+  	    hostListener.onClick(index, v.getId());
+	  }
+	});
+      }
+    }
+  }
+  
+  public interface ChildClickListener {
+    
+    public void onClick(int childIndex, int childId);
   }
 
+  private int getChildIndex(int id) {
+    for (int i = 0; i < getChildCount(); i++) {
+      if (id == getChildAt(i).getId()) {
+	return i;
+      }
+    }
+    return 0;
+  }
+  
   @Override
   public void addOption(int optionId, String text) {
     System.out.println("FWLayout couldn't handle command");
@@ -45,11 +80,32 @@ public class FWLayout extends LinearLayout implements NativeCommandHandler {
     }
   }
   
+  public void setChildListeners(final ChildClickListener listener) {
+    this.hostListener = listener;
+    childListeners = true;
+    for (int i = 0; i < getChildCount(); i++) {
+      View view = getChildAt(i);
+      if (view instanceof AdapterView) {
+	return;
+      } else {
+	System.out.println("set listener to " + view.getId());
+	view.setOnClickListener(new OnClickListener() {
+	  @Override
+	  public void onClick(View v) {
+  	    System.out.println("child click " + v.getId());
+  	    int index = getChildIndex(v.getId());
+  	    hostListener.onClick(index, v.getId());
+	  }
+	});
+      }
+    }
+  }
+  
   @Override
   public void setViewEnabled(Boolean enabled) {
     System.out.println("FWLayout couldn't handle command");
   }
-
+  
   @Override
   public void setStyle(String key, String value) {
     if (key.equals("gravity")) {
@@ -92,6 +148,13 @@ public class FWLayout extends LinearLayout implements NativeCommandHandler {
       setPadding(Integer.parseInt(value), getPaddingTop(), getPaddingRight(), getPaddingBottom());
     } else if (key.equals("padding-right")) {
       setPadding(getPaddingLeft(), getPaddingTop(), Integer.parseInt(value), getPaddingBottom());
+    } else if (key.equals("frame")) {
+      if (value.equals("light")) {
+	this.setBackgroundDrawable(frame.getResources().getDrawable(android.R.drawable.dialog_holo_light_frame));
+//	this.setDividerDrawable(frame.getResources().getDrawable(android.R.drawable.divider_horizontal_bright));
+      } else if (value.equals("dark")) {
+	this.setBackgroundDrawable(frame.getResources().getDrawable(android.R.drawable.alert_light_frame));
+      }
     }
   }
 
