9c0469716e72b7319fc0cbfb7a3ef651ebe76bca
Mikael Rekola <mikael.rekola@gmail.com>
2017-10-01 02:19:09 +0300

    fix fields

--- include/MobileAccount.h
+++ include/MobileAccount.h
@@ -3,17 +3,18 @@
 
 class MobileAccount {
  public:
-  MobileAccount(const char * _email, const char * _language, const char * _country = "") : email(_email), language(_language), country(_country) { }
+  MobileAccount() { }
+  MobileAccount(const std::string & _email, const std::string & _language, const std::string & _country)
+   : email(_email), language(_language), country(_country) { }
   
-  const char * getEmail() { return email; }
-  const char * getLanguage() { return language; }
-  const char * getCountry() { return country; }
+  const std::string & getEmail() const { return email; }
+  const std::string & getLanguage() const { return language; }
+  const std::string & getCountry() const { return country; }
 
  private:
- const char * email;
- const char * language;
- const char * country;
-
+  std::string email;
+  std::string language;
+  std::string country;
 };
 
 #endif
