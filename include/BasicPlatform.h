176a6d6b53c96a2c711b9945991e56dc5dc66bb7
Mikael Rekola <mikael.rekola@gmail.com>
2017-10-02 18:47:19 +0300

    refactor

--- include/BasicPlatform.h
+++ include/BasicPlatform.h
@@ -11,8 +11,8 @@
 
 class BasicThread : public PosixThread {
  public:
- BasicThread(int _id, FWPlatform * _platform, std::shared_ptr<Runnable> & _runnable)
-   : PosixThread(_id, _platform, _runnable) { }
+ BasicThread(int _id, PlatformThread * _parent_thread, FWPlatform * _platform, std::shared_ptr<Runnable> & _runnable)
+   : PosixThread(_id, _parent_thread, _platform, _runnable) { }
 
 #ifndef NO_CANVAS
   std::unique_ptr<canvas::ContextFactory> createContextFactory() const override {
@@ -22,12 +22,25 @@ class BasicThread : public PosixThread {
   std::unique_ptr<HTTPClientFactory> createHTTPClientFactory() const override {
     return std::unique_ptr<HTTPClientFactory>(new CurlClientFactory);
   }
+
+  std::shared_ptr<PlatformThread> createThread(std::shared_ptr<Runnable> & runnable) override {
+    auto & platform = getPlatform();
+    return std::make_shared<BasicThread>(platform.getNextThreadId(), this, &platform, runnable);
+  }
+
+  void sleep(double t) override {
+    usleep((unsigned int)(t * 1000000));
+  }
+
+  int sendCommand(const Command & command) override {
+    return 0;
+  }
 };
 
-class DummyThread : public PlatformThread {
+class BasicMainThread : public PlatformThread {
 public:
- DummyThread(int _id, FWPlatform * _platform, std::shared_ptr<Runnable> & _runnable)
-   : PlatformThread(_id, _platform, _runnable)
+ BasicMainThread(int _id, FWPlatform * _platform, std::shared_ptr<Runnable> & _runnable)
+   : PlatformThread(_id, 0, _platform, _runnable)
   {
     
   }
@@ -41,6 +54,19 @@ public:
     return std::unique_ptr<HTTPClientFactory>(new CurlClientFactory);
   }
 
+  std::shared_ptr<PlatformThread> createThread(std::shared_ptr<Runnable> & runnable) override {
+    auto & platform = getPlatform();
+    return std::make_shared<BasicThread>(platform.getNextThreadId(), this, &platform, runnable);
+  }
+
+  void sleep(double t) override {
+    usleep((unsigned int)(t * 1000000));
+  }
+
+  int sendCommand(const Command & command) override {
+    return 0;    
+  }
+
   bool start() override { return false; }
   bool testDestroy() override { return false; }
   void terminate() override { }
@@ -48,7 +74,7 @@ public:
 
 class BasicPlatform : public FWPlatform {
  public:
- BasicPlatform() : FWPlatform(1.0f) { }
+ BasicPlatform() { }
 
   std::string getBundleFilename(const char * filename) override {
     return filename;
@@ -56,18 +82,14 @@ class BasicPlatform : public FWPlatform {
   std::string getLocalFilename(const char * filename, FileType type) override {
     return filename;
   }
-  std::shared_ptr<PlatformThread> createThread(std::shared_ptr<Runnable> & runnable) override {
-    return std::make_shared<BasicThread>(getNextThreadId(), this, runnable);
-  }
   void pushEvent(int internal_id, const Event & ev) override {
 
   }
-  void sendCommand2(const Command & command) {
-    
-  }
-  std::string loadTextAsset(const char * filename) {
+  std::string loadTextAsset(const char * filename) override {
     return "";
   }
+  int startModal() override { return 0; }
+  void endModal() override { }
 };
 
 #endif
