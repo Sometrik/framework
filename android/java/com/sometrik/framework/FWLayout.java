33de97a0b456a7ac27ce57771aabdae89aa6a527
Mikael Rekola <mikael.rekola@gmail.com>
2017-10-10 20:16:03 +0300

    use ViewStyleManager for styles

--- android/java/com/sometrik/framework/FWLayout.java
+++ android/java/com/sometrik/framework/FWLayout.java
@@ -10,7 +10,7 @@ import android.graphics.Bitmap.Config;
 import android.graphics.Color;
 import android.graphics.drawable.BitmapDrawable;
 import android.graphics.drawable.GradientDrawable;
-import android.view.Gravity;
+import android.view.MotionEvent;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.FrameLayout;
@@ -22,23 +22,34 @@ public class FWLayout extends LinearLayout implements NativeCommandHandler {
   private FrameWork frame;
   private boolean childListeners = false;
   private ChildClickListener hostListener;
-  private GradientDrawable currentBackground = null;
+  ViewStyleManager normalStyle, activeStyle, currentStyle;
   
   public FWLayout(FrameWork frameWork) {
     super(frameWork);
     this.frame = frameWork;
-    setDividerDrawable(frame.getResources().getDrawable(android.R.drawable.divider_horizontal_bright));
+//    setDividerDrawable(frame.getResources().getDrawable(android.R.drawable.divider_horizontal_bright));
 //    setDividerDrawable(frame.getResources().getDrawable(android.R.drawable.divider_horizontal_textfield));
-  }
+    
+    final float scale = getContext().getResources().getDisplayMetrics().density;
+    this.normalStyle = currentStyle = new ViewStyleManager(scale, true);
+    this.activeStyle = new ViewStyleManager(scale, false);
 
-  private GradientDrawable createBackground() {
-    if (currentBackground == null) {
-      currentBackground = new GradientDrawable();
-      setBackground(currentBackground);
-    }
-    currentBackground.setColor(Color.parseColor("#ffffff")); // Changes this drawable to use a single color instead of a gradient
-    return currentBackground;
-  }    
+    final FWLayout layout = this;
+    
+    setOnTouchListener(new OnTouchListener() {
+      @Override
+      public boolean onTouch(View v, MotionEvent event) {
+	if (event.getAction() == MotionEvent.ACTION_DOWN) {
+	  layout.currentStyle = layout.activeStyle;
+	  layout.currentStyle.apply(layout);
+	} else if (event.getAction() == MotionEvent.ACTION_UP) {
+	  layout.currentStyle = layout.normalStyle;
+	  layout.currentStyle.apply(layout);
+	}
+	return false;
+      }
+    });
+  }
   
   @Override
   public int getElementId() {
@@ -133,66 +144,15 @@ public class FWLayout extends LinearLayout implements NativeCommandHandler {
   
   @Override
   public void setStyle(Selector selector, String key, String value) {
-    if (key.equals("gravity")) {
-      LinearLayout.LayoutParams params = (LayoutParams) getLayoutParams();
-      if (value.equals("bottom")) {
-	params.gravity = Gravity.BOTTOM;
-      } else if (value.equals("top")) {
-	params.gravity = Gravity.TOP;
-      } else if (value.equals("left")) {
-	params.gravity = Gravity.LEFT;
-      } else if (value.equals("right")) {
-	params.gravity = Gravity.RIGHT;
-      }
-      setLayoutParams(params);
-    } else if (key.equals("width")) {
-      if (getParent() instanceof ScrollView) {
-	FrameLayout.LayoutParams params = new FrameLayout.LayoutParams(LayoutParams.MATCH_PARENT, LayoutParams.MATCH_PARENT);
-	this.setLayoutParams(params);
-      } else {
-	      LinearLayout.LayoutParams params = (LayoutParams) getLayoutParams();
-	      if (value.equals("wrap-content")) {
-		params.width = LinearLayout.LayoutParams.WRAP_CONTENT;
-	      } else if (value.equals("match-parent")) {
-		params.width = LinearLayout.LayoutParams.MATCH_PARENT;
-	      } else {
-		final float scale = getContext().getResources().getDisplayMetrics().density;
-		int pixels = (int) (Integer.parseInt(value) * scale + 0.5f);
-		params.width = pixels;
-	      }
-	      setLayoutParams(params);
-      }
-    } else if (key.equals("height")) {
-      if (getParent() instanceof ScrollView) {
-	FrameLayout.LayoutParams params = new FrameLayout.LayoutParams(LayoutParams.MATCH_PARENT, LayoutParams.MATCH_PARENT);
-	this.setLayoutParams(params);
-      } else {
-	LinearLayout.LayoutParams params = (LayoutParams) getLayoutParams();
-	if (value.equals("wrap-content")) {
-	  params.height = LinearLayout.LayoutParams.WRAP_CONTENT;
-	} else if (value.equals("match-parent")) {
-	  params.height = LinearLayout.LayoutParams.MATCH_PARENT;
-	} else {
-
-	  final float scale = getContext().getResources().getDisplayMetrics().density;
-	  int pixels = (int) (Integer.parseInt(value) * scale + 0.5f);
-	  params.height = pixels;
-	}
-	setLayoutParams(params);
-      }
-    } else if (key.equals("weight")) {
-      LinearLayout.LayoutParams params = (LayoutParams) getLayoutParams();
-      params.weight = Integer.parseInt(value);
-      setLayoutParams(params);
-    } else if (key.equals("padding-top")) {
-      setPadding(getPaddingLeft(), Integer.parseInt(value), getPaddingRight(), getPaddingBottom());
-    } else if (key.equals("padding-bottom")) {
-      setPadding(getPaddingLeft(), getPaddingTop(), getPaddingRight(), Integer.parseInt(value));
-    } else if (key.equals("padding-left")) {
-      setPadding(Integer.parseInt(value), getPaddingTop(), getPaddingRight(), getPaddingBottom());
-    } else if (key.equals("padding-right")) {
-      setPadding(getPaddingLeft(), getPaddingTop(), Integer.parseInt(value), getPaddingBottom());
-    } else if (key.equals("frame")) {
+    if (selector == Selector.NORMAL) {
+      normalStyle.setStyle(key, value);
+      if (normalStyle == currentStyle) normalStyle.apply(this);
+    } else if (selector == Selector.ACTIVE) {
+      activeStyle.setStyle(key, value);      
+      if (activeStyle == currentStyle) activeStyle.apply(this);
+    }
+    
+    if (key.equals("frame")) {
       if (value.equals("light")) {
 	this.setBackgroundDrawable(frame.getResources().getDrawable(android.R.drawable.dialog_holo_light_frame));
 //	this.setDividerDrawable(frame.getResources().getDrawable(android.R.drawable.divider_horizontal_bright));
@@ -201,44 +161,12 @@ public class FWLayout extends LinearLayout implements NativeCommandHandler {
       }
     } else if (key.equals("divider")) {
       if (value.equals("middle")) {
-	this.setShowDividers(LinearLayout.SHOW_DIVIDER_MIDDLE);
+	// this.setShowDividers(LinearLayout.SHOW_DIVIDER_MIDDLE);
       } else if (value.equals("end")) {
-	this.setShowDividers(LinearLayout.SHOW_DIVIDER_END);
+	// this.setShowDividers(LinearLayout.SHOW_DIVIDER_END);
       } else if (value.equals("beginning")) {
-	this.setShowDividers(LinearLayout.SHOW_DIVIDER_BEGINNING);
+	// this.setShowDividers(LinearLayout.SHOW_DIVIDER_BEGINNING);
       }
-    } else if (key.equals("background-color")) {
-      this.setBackgroundColor(Color.parseColor(value));
-    } else if (key.equals("margin-right")) {
-      LinearLayout.LayoutParams params = (LayoutParams) getLayoutParams();
-      params.rightMargin = Integer.parseInt(value);
-      setLayoutParams(params);
-    } else if (key.equals("margin-left")) {
-      LinearLayout.LayoutParams params = (LayoutParams) getLayoutParams();
-      params.leftMargin = Integer.parseInt(value);
-      setLayoutParams(params);
-    } else if (key.equals("margin-top")) {
-      LinearLayout.LayoutParams params = (LayoutParams) getLayoutParams();
-      params.topMargin = Integer.parseInt(value);
-      setLayoutParams(params);
-    } else if (key.equals("margin-bottom")) {
-      LinearLayout.LayoutParams params = (LayoutParams) getLayoutParams();
-      params.bottomMargin = Integer.parseInt(value);
-      setLayoutParams(params);
-    } else if (key.equals("shadow")) {
-      setElevation(Integer.parseInt(value));
-    } else if (key.equals("border")) {
-      if (value.equals("none")) {
-	setBackgroundResource(0);
-      } else {
-	GradientDrawable gd = createBackground();
-	gd.setStroke(1, Color.parseColor(value));
-      }
-    } else if (key.equals("border-radius")) {
-      final float scale = getContext().getResources().getDisplayMetrics().density;
-      int pixels = (int) (Integer.parseInt(value) * scale + 0.5f);
-      GradientDrawable gd = createBackground();
-      gd.setCornerRadius(pixels);      
     }
   }
 
