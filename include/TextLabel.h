57e472117a13830edd95e4aba1984090fa2621a4
Mikael Rekola <mikael.rekola@gmail.com>
2017-10-13 02:35:28 +0300

    rename field

--- include/TextLabel.h
+++ include/TextLabel.h
@@ -6,26 +6,26 @@
 
 class TextLabel : public Element {
  public:
- TextLabel(const std::string & _text, unsigned int _flags = 0, bool _autolink = false) : Element(0, _flags), text(_text), autolink(_autolink) { }
- TextLabel(const std::string & _text, const std::string & _url) : Element(0, 1), text(_text), url(_url), autolink(false) { }
+ TextLabel(const std::string & _value, unsigned int _flags = 0, bool _autolink = false) : Element(0, _flags), value(_value), autolink(_autolink) { }
+ TextLabel(const std::string & _value, const std::string & _url) : Element(0, 1), value(_value), url(_url), autolink(false) { }
 
   bool isA(const std::string & className) const override {
     if (className == "TextLabel") return true;
     return Element::isA(className);
   }
 
-  void text(const std::string & _text) override {
-    text = _text;
+  void text(const std::string & _value) override {
+    value = _value;
     
     Command c(Command::SET_TEXT_VALUE, getInternalId());
-    c.setTextValue(text);
+    c.setTextValue(value);
     sendCommand(c);
   }
 
  protected:
   void create() override {
     Command c(Command::CREATE_TEXT, getParentInternalId(), getInternalId());
-    c.setTextValue(text);
+    c.setTextValue(value);
     c.setTextValue2(url);
     c.setValue(autolink ? 1 : 0);
     c.setFlags(getFlags());
@@ -33,7 +33,7 @@ class TextLabel : public Element {
   }
 
  private:
-  std::string text;
+  std::string value;
   std::string url;
   bool autolink;
 };
