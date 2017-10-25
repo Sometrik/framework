4ccdce579cbc762617df9c0fa3092c3dff34ffd5
MSuni <mikko.suni@hotmail.com>
2017-10-25 11:37:00 +0300

    Remove bad default icon. Use system icon

--- android/java/com/sometrik/framework/FWNotification.java
+++ android/java/com/sometrik/framework/FWNotification.java
@@ -1,39 +1,52 @@
 package com.sometrik.framework;
 
+import java.util.List;
+
 import com.sometrik.framework.NativeCommand.Selector;
 
-import android.R;
 import android.app.Notification;
 import android.app.NotificationManager;
 import android.app.PendingIntent;
 import android.content.Intent;
+import android.content.pm.PackageInfo;
 import android.graphics.Bitmap.Config;
+import android.graphics.drawable.Drawable;
 import android.view.View;
 
 public class FWNotification extends Notification.Builder implements NativeCommandHandler {
 
   private int id;
   private FrameWork frame;
-  
-  private static final int iconId = com.sometrik.lister.R.drawable.chaff_logo;
-  
+
   public FWNotification(FrameWork frame, String title, String text) {
     super(frame);
     this.frame = frame;
     Intent intent = new Intent(frame, FrameWork.class);
     PendingIntent pIntent = PendingIntent.getActivity(frame, (int) System.currentTimeMillis(), intent, 0);
-
     setContentTitle(title);
     setContentText(text);
     setContentIntent(pIntent);
-    setSmallIcon(iconId);
+    setSmallIcon(android.R.drawable.arrow_down_float);
+    
+    //Attempt at getting icon from application
+//    List<PackageInfo> packs = frame.getPackageManager().getInstalledPackages(0);
+//
+//    System.out.println("packs size: " + packs.size());
+//
+//    for (PackageInfo p : packs) {
+//      System.out.println("packs 1: " + p.applicationInfo.icon);
+//      if (p.applicationInfo.icon != 0) {
+//	setSmallIcon(p.applicationInfo.icon);
+//	break;
+//      }
+//    }
     setAutoCancel(true);
   }
-  
+
   @Override
   public void onScreenOrientationChange(boolean isLandscape) {
     // TODO Auto-generated method stub
-    
+
   }
 
   @Override
@@ -44,49 +57,49 @@ public class FWNotification extends Notification.Builder implements NativeComman
   @Override
   public void addOption(int optionId, String text) {
     // TODO Auto-generated method stub
-    
+
   }
 
   @Override
   public void addColumn(String text, int columnType) {
     // TODO Auto-generated method stub
-    
+
   }
 
   @Override
   public void addData(String text, int row, int column, int sheet) {
     // TODO Auto-generated method stub
-    
+
   }
 
   @Override
   public void setValue(String v) {
     // TODO Auto-generated method stub
-    
+
   }
 
   @Override
   public void setImage(byte[] bytes, int width, int height, Config config) {
     // TODO Auto-generated method stub
-    
+
   }
 
   @Override
   public void setValue(int v) {
     // TODO Auto-generated method stub
-    
+
   }
 
   @Override
   public void reshape(int value, int size) {
     // TODO Auto-generated method stub
-    
+
   }
 
   @Override
   public void reshape(int size) {
     // TODO Auto-generated method stub
-    
+
   }
 
   @Override
@@ -94,28 +107,28 @@ public class FWNotification extends Notification.Builder implements NativeComman
     if (visible) {
       Notification notif = getNotification();
       NotificationManager notificationManager = (NotificationManager) frame.getSystemService(FrameWork.NOTIFICATION_SERVICE);
-      notificationManager.notify(4535312, notif);
+      notificationManager.notify(234235, notif);
     }
   }
 
   @Override
   public void setStyle(Selector selector, String key, String value) {
     // TODO Auto-generated method stub
-    
+
   }
 
   @Override
   public void setError(boolean hasError, String errorText) {
     // TODO Auto-generated method stub
-    
+
   }
 
   @Override
   public void clear() {
     // TODO Auto-generated method stub
-    
+
   }
-  
+
   public void setId(int id) {
     this.id = id;
   }
@@ -123,13 +136,13 @@ public class FWNotification extends Notification.Builder implements NativeComman
   @Override
   public void flush() {
     // TODO Auto-generated method stub
-    
+
   }
 
   @Override
   public void deinitialize() {
     // TODO Auto-generated method stub
-    
+
   }
 
   @Override
