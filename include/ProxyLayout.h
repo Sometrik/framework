ddf4bc2bfbb1c450cb73c0c7a6a698bd0c2139e8
Mikael Rekola <mikael.rekola@gmail.com>
2017-11-07 00:21:36 +0200

    add templates to ProxyLayout

--- include/ProxyLayout.h
+++ include/ProxyLayout.h
@@ -4,6 +4,7 @@
 #include <Element.h>
 #include <ScrollChangedEvent.h>
 
+template <class T1, class T2>
 class ProxyLayout : public Element {
 public:
   ProxyLayout(int _initial_visible_count, int _id = 0) : Element(_id), max_visible_count(_initial_visible_count) { }
@@ -18,9 +19,9 @@ public:
     all_keys.clear();
   }
 
-  void addProxy(const std::string & key, const std::string & data) {
+  void addProxy(const T1 & key, const T2 & data) {
     unsigned int pos = all_keys.size();
-    all_keys.push_back(std::pair<std::string, std::string>(key, data));
+    all_keys.push_back(std::make_pair(key, data));
 
     if (pos < max_visible_count) {
       showKey(pos, key, data);
@@ -42,19 +43,21 @@ public:
     if (ev.getHeight() != current_content_height && ev.getScrollRem() < 10) {
       current_content_height = ev.getHeight();
 
-      begin();
-      unsigned int c = 10;
-      for (unsigned int i = max_visible_count; i < max_visible_count + c && i < all_keys.size(); i++) {
-	auto & p = all_keys[i];
-	showKey(i, p.first, p.second);
+      if (all_keys.size() > max_visible_count) {
+        begin();
+        unsigned int c = 10;
+        for (unsigned int i = max_visible_count; i < max_visible_count + c && i < all_keys.size(); i++) {
+          auto & p = all_keys[i];
+          showKey(i, p.first, p.second);
+        }
+        max_visible_count += c;
+        commit();
       }
-      max_visible_count += c;
-      commit();
     }
   }
 
 protected:
-  void showKey(unsigned int pos, const std::string & key, const std::string & data) {
+  void showKey(unsigned int pos, const T1 & key, const T2 & data) {
     visible_keys.insert(key);
       
     auto orig = getContent(key);
@@ -71,10 +74,10 @@ protected:
     }
   }
   
-  virtual std::shared_ptr<Element> createContent(const std::string & key, const std::string & data) = 0;
-  virtual void updateContent(Element & e, const std::string & key, const std::string & data) = 0;
+  virtual std::shared_ptr<Element> createContent(const T1 & key, const T2 & data) = 0;
+  virtual void updateContent(Element & e, const T1 & key, const T2 & data) = 0;
   
-  std::shared_ptr<Element> getContent(const std::string & key) {
+  std::shared_ptr<Element> getContent(const T1 & key) {
     auto it = content.find(key);
     if (it != content.end()) {
       return it->second;
@@ -94,9 +97,9 @@ protected:
 private:
   int max_visible_count;
   int current_content_height = 0;
-  std::unordered_map<std::string, std::shared_ptr<Element> > content;
-  std::unordered_set<std::string> visible_keys;
-  std::vector<std::pair<std::string, std::string> > all_keys;
+  std::unordered_map<T1, std::shared_ptr<Element> > content;
+  std::unordered_set<T1> visible_keys;
+  std::vector<std::pair<T1, T2> > all_keys;
 };
 
 #endif
