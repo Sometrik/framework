a6fd1a912c48fb2cca7111f71e4a0fd79a73dbcb
Mikael Rekola <mikael.rekola@gmail.com>
2017-11-09 05:39:47 +0200

    remove sstream

--- include/shims.h
+++ include/shims.h
@@ -2,8 +2,6 @@
 #define _SHIMS_H_
 
 #include <string>
-#include <cstring>
-#include <sstream>
 #include <stdlib.h>
 
 inline void assignBoundVar(std::string & a, const std::string & b) { a = b; }
@@ -11,11 +9,7 @@ inline void assignBoundVar(int & a, const std::string & b) { a = atoi(b.c_str())
 inline void assignBoundVar(bool & a, const std::string & b) { a = !b.empty() && b != "0" && b != "false"; }
 inline void assignBoundVar(float & a, const std::string & b) { a = atof(b.c_str()) != 0; }
 
-inline void assignBoundVar(std::string & a, bool b) {
-  std::stringstream stream;
-  stream << b;
-  a = stream.str();
-}
+inline void assignBoundVar(std::string & a, bool b) { a = b ? "1" : "0"; }
 inline void assignBoundVar(int & a, bool b) { a = int(b); }
 inline void assignBoundVar(bool & a, bool b) { a = b; }
 inline void assignBoundVar(float & a, bool b) { a = (int)b; }
