7bac1a024e4245245e93261c41a17e2eeb9e97cf
Mikael Rekola <mikael.rekola@gmail.com>
2017-11-16 05:55:57 +0200

    make the ActionSheet work

--- android/java/com/sometrik/framework/FWActionSheet.java
+++ android/java/com/sometrik/framework/FWActionSheet.java
@@ -4,124 +4,82 @@ import com.sometrik.framework.NativeCommand.Selector;
 
 import android.graphics.Bitmap;
 import android.view.Menu;
-import android.view.MenuInflater;
 import android.view.View;
 import android.widget.PopupMenu;
 
 public class FWActionSheet extends PopupMenu implements NativeCommandHandler {
 
   private FrameWork frame;
-  private MenuInflater inflater;
-  private Menu menu;
   private int id;
+  private boolean hasEntries = false;
   
   public FWActionSheet(FrameWork frame, View anchor, int id) {
     super(frame, anchor);
     this.id = id;
-    inflater = new MenuInflater(frame);
-    menu = getMenu();
+   
+    // Add dummy entry, or the menu will not have any width
+    getMenu().add("No menu entries");
+    show();
   }
 
   @Override
-  public void onScreenOrientationChange(boolean isLandscape) {
-    // TODO Auto-generated method stub
-    
-  }
+  public void onScreenOrientationChange(boolean isLandscape) { }
 
   @Override
-  public void addChild(View view) {
-    // TODO Auto-generated method stub
-    
-  }
+  public void addChild(View view) { }
 
   @Override
   public void addOption(int optionId, String text) {
-    menu.add(Menu.NONE, optionId, Menu.NONE, text);
-  }
-
-  @Override
-  public void addColumn(String text, int columnType) {
-    // TODO Auto-generated method stub
-    
-  }
-
-  @Override
-  public void addData(String text, int row, int column, int sheet) {
-    // TODO Auto-generated method stub
-    
-  }
-
-  @Override
-  public void setValue(String v) {
-    // TODO Auto-generated method stub
-    
-  }
-
-  @Override
-  public void setBitmap(Bitmap bitmap) {
-    // TODO Auto-generated method stub
-    
-  }
-
-  @Override
-  public void addImageUrl(String url, int width, int height) {
-    // TODO Auto-generated method stub
-    
-  }
-
-  @Override
-  public void setValue(int v) {
-    if (v > 0) {
-      show();
+    if (!hasEntries) {
+      getMenu().clear();
+      hasEntries = true;      
     }
+    System.out.println("ActionSheet adding option " + optionId + " " + text);
+    getMenu().add(Menu.NONE, optionId, Menu.NONE, text);
   }
 
   @Override
-  public void reshape(int value, int size) {
-    // TODO Auto-generated method stub
-    
-  }
+  public void addColumn(String text, int columnType) { }
 
   @Override
-  public void reshape(int size) {
-    // TODO Auto-generated method stub
-    
-  }
+  public void addData(String text, int row, int column, int sheet) { }
 
   @Override
-  public void setViewVisibility(boolean visible) {
-    // TODO Auto-generated method stub
-    
-  }
+  public void setValue(String v) { }
 
   @Override
-  public void setStyle(Selector selector, String key, String value) {
-    // TODO Auto-generated method stub
-    
-  }
+  public void setBitmap(Bitmap bitmap) { }
 
   @Override
-  public void setError(boolean hasError, String errorText) {
-    // TODO Auto-generated method stub
-    
-  }
+  public void addImageUrl(String url, int width, int height) { }
 
   @Override
-  public void clear() {
-    // TODO Auto-generated method stub
-    
-  }
+  public void setValue(int v) { }
 
   @Override
-  public void flush() {
-    // TODO Auto-generated method stub
-    
-  }
+  public void reshape(int value, int size) { }
+
+  @Override
+  public void reshape(int size) { }
+
+  @Override
+  public void setViewVisibility(boolean visible) { }
+
+  @Override
+  public void setStyle(Selector selector, String key, String value) { }
+
+  @Override
+  public void setError(boolean hasError, String errorText) { }
+
+  @Override
+  public void clear() { }
+
+  @Override
+  public void flush() { }
 
   @Override
   public void deinitialize() {
-    // TODO Auto-generated method stub
-    
+    dismiss();
   }
 
   @Override
