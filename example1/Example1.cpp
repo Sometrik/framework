74cb774a1a717940ec8ae9fa9f286c4cb33af884
Mikael Rekola <mikael.rekola@gmail.com>
2017-11-22 16:58:27 +0200

    add more elements to example

--- example1/Example1.cpp
+++ example1/Example1.cpp
@@ -4,6 +4,7 @@
 #include <TextLabel.h>
 #include <LinearLayout.h>
 #include <Button.h>
+#include <ScrollLayout.h>
 
 #include <iostream>
 
@@ -16,9 +17,27 @@ Example1::Example1() : FWApplication("com.sometrik.example1")
 
   auto layout = std::make_shared<LinearLayout>(FW_VERTICAL);
   view->addChild(layout);
+
+  auto title = make_shared<TextLabel>("Hello World!");
+  title->style("font-size", 20);
+  layout->addChild(title);
+
+  auto buttonLayout = std::make_shared<LinearLayout>(FW_HORIZONTAL);
+  buttonLayout->addChild(make_shared<Button>("Click me!"));
+  buttonLayout->addChild(make_shared<Button>("Cancel"));
+  buttonLayout->addChild(make_shared<Button>("Button"));
+  layout->addChild(buttonLayout);
+
+  auto scrollLayout = std::make_shared<ScrollLayout>();
+  scrollLayout->style("min-height", 200);
+  layout->addChild(scrollLayout);
+  auto scrollContent = std::make_shared<LinearLayout>(FW_VERTICAL);
+  scrollLayout->addChild(scrollContent);
   
-  layout->addChild(make_shared<TextLabel>("Hello World!"));
-  layout->addChild(make_shared<Button>("Click me!"));
+  for (int i = 0; i < 100; i++) {
+    scrollContent->addChild(std::make_shared<TextLabel>("Number: " + to_string(i)));
+  }
+  
 }
 
 FWApplication * applicationMain() {
