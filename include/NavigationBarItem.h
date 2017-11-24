799c606f8a415833417a92904100f9d5182ebb5e
Mikael Rekola <mikael.rekola@gmail.com>
2017-11-24 13:52:58 +0200

    add items, and fix typo

---
+++ include/NavigationBarItem.h
@@ -0,0 +1,27 @@
+#ifndef _NAVIGATIONBARITEM_H_
+#define _NAVIGATIONBARITEM_H_
+
+#include <Button.h>
+
+class NavigationBarItem : public Button {
+ public:
+ NavigationBarItem(const std::string & _label, int _id = 0)
+   : Button(_label, _id) { }
+
+  bool isA(const std::string & className) const override {
+    if (className == "NavigationBarItem") return true;
+    return Button::isA(className);
+  }
+
+protected:
+  void create() override {
+    Command c(Command::CREATE_NAVIGATIONBAR_ITEM, getParentInternalId(), getInternalId());
+    c.setTextValue(label);
+    sendCommand(c);
+  }
+
+ private:
+  std::string label;
+};
+
+#endif
