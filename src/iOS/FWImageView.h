29536bbb3fd684146a53d875b8cf76ea46274d44
Mikael Rekola <mikael.rekola@gmail.com>
2017-12-10 01:05:01 +0200

    add method layoutSubviews

--- src/iOS/FWImageView.h
+++ src/iOS/FWImageView.h
@@ -1,5 +1,5 @@
 #import <UIKit/UIKit.h>
 
 @interface FWImageView : UIImageView
-
+- (void)layoutSubviews;
 @end
--- src/iOS/FWImageView.mm
+++ src/iOS/FWImageView.mm
@@ -2,4 +2,9 @@
 
 @implementation FWImageView
 
+- (void)layoutSubviews {
+    [super layoutSubviews];
+    NSLog(@"FWImageView, layoutSubview(): %f %f", self.frame.size.width, self.frame.size.height);
+}
+
 @end
