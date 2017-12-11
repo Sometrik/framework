398dd76e0f7ad99fb98896cdd99714f892b762b6
Mikael Rekola <mikael.rekola@gmail.com>
2017-12-11 23:33:01 +0200

    add random photo

--- example1/ImageDialog.h
+++ example1/ImageDialog.h
@@ -18,7 +18,7 @@ public:
     auto label = std::make_shared<TextLabel>("Hello!");
     layout->addChild(label);
     
-    auto image = std::make_shared<ImageElement>();
+    auto image = std::make_shared<ImageElement>("https://picsum.photos/200/300/?random");
     image->style("width", 128);
     image->style("height", 256);
     image->style("border", "#ffffff");
