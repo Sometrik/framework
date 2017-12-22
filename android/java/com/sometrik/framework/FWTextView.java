548e71593c1350d901cc5558cfb34dd98ac09692
MSuni <mikko.suni@hotmail.com>
2017-12-22 14:57:16 +0200

    Fix FWtextView link clicking

--- android/java/com/sometrik/framework/FWTextView.java
+++ android/java/com/sometrik/framework/FWTextView.java
@@ -15,69 +15,74 @@ public class FWTextView extends TextView implements NativeCommandHandler {
 
   private FrameWork frame;
   ViewStyleManager normalStyle, activeStyle, currentStyle, linkStyle;
-  
+
   public FWTextView(final FrameWork frame, boolean autolink) {
     super(frame);
     this.frame = frame;
-    
+
     final float scale = getContext().getResources().getDisplayMetrics().density;
     this.normalStyle = currentStyle = new ViewStyleManager(frame.bitmapCache, scale, true);
     this.activeStyle = new ViewStyleManager(frame.bitmapCache, scale, false);
     this.linkStyle = new ViewStyleManager(frame.bitmapCache, scale, false);
+
+//    this.setClickable(false);
+//    this.setLongClickable(false);
+//    this.setFocusable(false);
 
     if (autolink) {
       this.setLinksClickable(false);
       this.setMovementMethod(new TextViewLinkHandler() {
 	@Override
 	public void onLinkClick(String url) {
-	  System.out.println("link clicked");
+	  System.out.println("link clicked " + url);
 	  frame.launchBrowser(url);
 	}
       });
       this.setAutoLinkMask(15);
+      this.setFocusable(true);
     }
 
     this.setClickable(false);
     this.setLongClickable(false);
-    this.setFocusable(false);
   }
-  
+
   public abstract class TextViewLinkHandler extends LinkMovementMethod {
 
     public boolean onTouchEvent(TextView widget, Spannable buffer, MotionEvent event) {
-      if (event.getAction() != MotionEvent.ACTION_UP)
+      int x = (int) event.getX();
+      int y = (int) event.getY();
+
+      x -= widget.getTotalPaddingLeft();
+      y -= widget.getTotalPaddingTop();
+
+      x += widget.getScrollX();
+      y += widget.getScrollY();
+
+      Layout layout = widget.getLayout();
+      int line = layout.getLineForVertical(y);
+      int off = layout.getOffsetForHorizontal(line, x);
+
+      URLSpan[] link = buffer.getSpans(off, off, URLSpan.class);
+      if (link.length != 0) {
+	if (event.getAction() == MotionEvent.ACTION_UP) {
+	  onLinkClick(link[0].getURL());
+	}
+	return true;
+      } else {
 	return false;
-        
-
-        int x = (int) event.getX();
-        int y = (int) event.getY();
-
-        x -= widget.getTotalPaddingLeft();
-        y -= widget.getTotalPaddingTop();
-
-        x += widget.getScrollX();
-        y += widget.getScrollY();
-
-        Layout layout = widget.getLayout();
-        int line = layout.getLineForVertical(y);
-        int off = layout.getOffsetForHorizontal(line, x);
-
-        URLSpan[] link = buffer.getSpans(off, off, URLSpan.class);
-        if (link.length != 0) {
-            onLinkClick(link[0].getURL());
-            return true;
-        }
-        return false;
+      }
     }
 
     abstract public void onLinkClick(String url);
-}
-    
+  }
+
   @Override
-  public void addChild(View view) { }
-  
+  public void addChild(View view) {
+  }
+
   @Override
-  public void addOption(int optionId, String text) { }
+  public void addOption(int optionId, String text) {
+  }
 
   @Override
   public void setValue(String v) {
@@ -85,14 +90,15 @@ public class FWTextView extends TextView implements NativeCommandHandler {
   }
 
   @Override
-  public void setValue(int v) { }
+  public void setValue(int v) {
+  }
 
   @Override
   public void setStyle(Selector selector, String key, String value) {
     if (selector == Selector.NORMAL) {
       normalStyle.setStyle(key, value);
     } else if (selector == Selector.ACTIVE) {
-      activeStyle.setStyle(key, value);      
+      activeStyle.setStyle(key, value);
     } else if (selector == Selector.LINK) {
       linkStyle.setStyle(key, value);
     }
@@ -104,12 +110,13 @@ public class FWTextView extends TextView implements NativeCommandHandler {
 
   @Override
   public void applyStyles() {
-    currentStyle.apply(this);  
+    currentStyle.apply(this);
     linkStyle.applyLinkColor(this);
   }
-  
+
   @Override
-  public void setError(boolean hasError, String errorText) { }
+  public void setError(boolean hasError, String errorText) {
+  }
 
   @Override
   public int getElementId() {
@@ -117,14 +124,16 @@ public class FWTextView extends TextView implements NativeCommandHandler {
   }
 
   @Override
-  public void onScreenOrientationChange(boolean isLandscape) { }
+  public void onScreenOrientationChange(boolean isLandscape) {
+  }
 
   @Override
-  public void addData(String text, int row, int column, int sheet) { }
+  public void addData(String text, int row, int column, int sheet) {
+  }
 
   @Override
   public void setViewVisibility(boolean visibility) {
-    if (visibility){
+    if (visibility) {
       this.setVisibility(VISIBLE);
     } else {
       this.setVisibility(INVISIBLE);
@@ -132,26 +141,34 @@ public class FWTextView extends TextView implements NativeCommandHandler {
   }
 
   @Override
-  public void clear() { }
+  public void clear() {
+  }
 
   @Override
-  public void flush() { }
+  public void flush() {
+  }
 
   @Override
-  public void addColumn(String text, int columnType) { }
+  public void addColumn(String text, int columnType) {
+  }
 
   @Override
-  public void reshape(int value, int size) { }
+  public void reshape(int value, int size) {
+  }
 
   @Override
-  public void setBitmap(Bitmap bitmap) { }
+  public void setBitmap(Bitmap bitmap) {
+  }
 
   @Override
-  public void reshape(int size) { }
+  public void reshape(int size) {
+  }
 
   @Override
-  public void deinitialize() { }
+  public void deinitialize() {
+  }
 
   @Override
-  public void addImageUrl(String url, int width, int height) { }
+  public void addImageUrl(String url, int width, int height) {
+  }
 }
