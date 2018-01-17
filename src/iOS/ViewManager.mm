12ce73d2244ad31e7247510bf83ded958d589e1a
Mikael Rekola <mikael.rekola@gmail.com>
2018-01-18 01:28:34 +0200

    parse alpha channel. Fixes #475

--- src/iOS/ViewManager.mm
+++ src/iOS/ViewManager.mm
@@ -299,11 +299,15 @@
 }
 
 - (UIColor *)colorFromString:(NSString *)hexString {
-  unsigned rgbValue = 0;
+  unsigned int rgbValue = 0;
   NSScanner *scanner = [NSScanner scannerWithString:hexString];
   [scanner setScanLocation:1]; // bypass '#' character
   [scanner scanHexInt:&rgbValue];
-  return [UIColor colorWithRed:((rgbValue & 0xFF0000) >> 16)/255.0 green:((rgbValue & 0xFF00) >> 8)/255.0 blue:(rgbValue & 0xFF)/255.0 alpha:1.0];
+  if (rgbValue <= 0xffffff) {
+    return [UIColor colorWithRed:((rgbValue & 0xFF0000) >> 16)/255.0 green:((rgbValue & 0xFF00) >> 8)/255.0 blue:(rgbValue & 0xFF)/255.0 alpha:1.0];
+  } else {
+    return [UIColor colorWithRed:((rgbValue & 0xFF0000) >> 16)/255.0 green:((rgbValue & 0xFF00) >> 8)/255.0 blue:(rgbValue & 0xFF)/255.0 alpha:((rgbValue & 0xFF000000) >> 24)/255.0];
+  }
 }
 
 - (UIFont *)createFont:(UIFont *)currentFont {
