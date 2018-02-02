fd4c96611d26648cf1965f9ae91a8a8cff859c1f
Mikael Rekola <mikael.rekola@gmail.com>
2018-02-02 02:47:23 +0200

    fit image

--- example1/ImageDialog.h
+++ example1/ImageDialog.h
@@ -19,7 +19,7 @@ public:
     layout->addChild(label);
     
     auto image = std::make_shared<ImageElement>();
-    image->style("width", 128);
+    image->style("width", "match-parent");
     image->style("height", 256);
     image->style("border", "#ffffff");
     layout->addChild(image);
