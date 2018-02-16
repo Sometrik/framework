8e681c5fe6f86802e3618fe4bda251cefee71a46
Mikael Rekola <mikael.rekola@gmail.com>
2018-02-16 15:27:28 +0200

    update appearance

--- example1/FrontView.h
+++ example1/FrontView.h
@@ -10,8 +10,16 @@
 class FrontView : public FrameView {
 public:
     FrontView(int _id = 0) : FrameView("Front page", _id) {
+        style("background", "linear-gradient(#ff0000, #ffff00)");
         auto & layout = addChild(std::make_shared<LinearLayout>(FW_VERTICAL));
-        layout.addChild(std::make_shared<Button>("Click here!", ID_SHOW_FRONT_PAGE));
+	layout.style("margin", 25);
+	layout.style("padding", 25);
+        auto & button = layout.addChild(std::make_shared<Button>("Click here!", ID_SHOW_FRONT_PAGE));
+	button.style("background", "#ffffff");
+	button.style("border-radius", 5);
+	button.style("margin", 25);
+	button.style("color", "#000000");
+	button.style("padding", 25);
     }
 private:
     
