1c59087c6a08ccbe2b3390d6f2dc97713d6ea46a
Mikael Rekola <mikael.rekola@gmail.com>
2018-02-25 05:13:17 +0200

    implement SET_INT_VALUE for Picker. Fixes #567

--- src/iOS/ViewManager.m
+++ src/iOS/ViewManager.m
@@ -58,10 +58,9 @@ LinearLayoutItemMargin LLMakeMargin(CGFloat top, CGFloat left, CGFloat bottom, C
     } else if ([self.view isKindOfClass:UIPageControl.class]) {
         UIPageControl * pageControl = (UIPageControl *)self.view;
         pageControl.currentPage = value;
-    } else if ([self.view isKindOfClass:UIPickerView.class]) {
-    	UIPickerView * picker = (UIPickerView *)self.view;
-        [picker reloadAllComponents];
-    	[picker selectRow:value inComponent:0 animated:NO];
+    } else if ([self.view isKindOfClass:FWPicker.class]) {
+    	FWPicker * picker = (FWPicker *)self.view;
+    	[picker setSelection:value];
     }
 }
 
