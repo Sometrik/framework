2f372bbc0e69cf778b72be173ad13fa1e5284e89
Mikael Rekola <mikael.rekola@gmail.com>
2018-02-25 05:50:47 +0200

    update layout when label bounds get smaller than preferredMaxLayoutWidth. Fixes #566. Fixes #554

--- src/iOS/PaddedLabel.h
+++ src/iOS/PaddedLabel.h
@@ -1,7 +1,8 @@
 #import <UIKit/UIKit.h>
 
 @interface PaddedLabel : UILabel
-
+- (void)setBounds:(CGRect)bounds;
+- (CGSize)intrinsicContentSize;
 @property (nonatomic, assign) UIEdgeInsets edgeInsets;
 
 @end
--- src/iOS/PaddedLabel.m
+++ src/iOS/PaddedLabel.m
@@ -1,18 +1,69 @@
 #import "PaddedLabel.h"
 
+#import "FrameLayoutView.h"
+#import "LinearLayoutView.h"
+
 @implementation PaddedLabel
 
-- (id)initWithFrame:(CGRect)frame{
-  self = [super initWithFrame:frame];
-  if (self) {
-    self.edgeInsets = UIEdgeInsetsMake(0, 0, 0, 0);
-  }
-  return self;
+- (id)init {
+    self = [super init];
+    if (self) {
+        self.edgeInsets = UIEdgeInsetsMake(0, 0, 0, 0);
+    }
+    return self;
+}
+
+- (id)initWithFrame:(CGRect)frame {
+    self = [super initWithFrame:frame];
+    if (self) {
+        self.edgeInsets = UIEdgeInsetsMake(0, 0, 0, 0);
+    }
+    return self;
 }
 
 - (void)drawTextInRect:(CGRect)rect {
   [super drawTextInRect:UIEdgeInsetsInsetRect(rect, self.edgeInsets)];
 }
+
+- (void)setBounds:(CGRect)bounds {
+  if (bounds.size.width < self.preferredMaxLayoutWidth) {
+      NSLog(@"Bounds changed %f => %f", self.bounds.size.width, bounds.size.width);
+    [super setBounds:bounds];
+#if 0
+    [self setNeedsUpdateConstraints];
+#else
+    self.preferredMaxLayoutWidth = self.bounds.size.width;
+    [self relayoutAll];
+#endif
+  } else {
+    [super setBounds:bounds];
+  }
+}
+
+- (void)relayoutAll {
+    UIView * view = self.superview;
+    while (view != nil && ([view isKindOfClass:FrameLayoutView.class] || [view isKindOfClass:LinearLayoutView.class])) {
+        [view setNeedsLayout];
+        view = view.superview;
+    }
+}
+
+#if 0
+- (void)updateConstraints {
+  if (self.preferredMaxLayoutWidth != self.bounds.size.width) {
+    self.preferredMaxLayoutWidth = self.bounds.size.width;
+  }
+  [super updateConstraints];
+}
+#endif
+
+#if 0
+- (void)layoutSubviews {
+    [super layoutSubviews];
+    self.preferredMaxLayoutWidth = self.frame.size.width;
+    [self.superview layoutIfNeeded];
+}
+#endif
 
 - (CGSize)intrinsicContentSize
 {
