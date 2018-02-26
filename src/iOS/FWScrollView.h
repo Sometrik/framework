513b699f443998170ce575e9f25691f86afbe493
Mikael Rekola <mikael.rekola@gmail.com>
2018-02-26 04:56:41 +0200

    add constraints etc.

--- src/iOS/FWScrollView.h
+++ src/iOS/FWScrollView.h
@@ -2,4 +2,8 @@
 
 @interface FWScrollView : UIScrollView
 - (void)layoutSubviews;
+@property NSLayoutConstraint *topConstraint;
+@property NSLayoutConstraint *leftConstraint;
+@property NSLayoutConstraint *widthConstraint;
+@property NSLayoutConstraint *heightConstraint;
 @end
--- src/iOS/FWScrollView.mm
+++ src/iOS/FWScrollView.mm
@@ -1,10 +1,17 @@
 #import "FWScrollView.h"
 
+#import "FrameLayoutView.h"
+#import "LinearLayoutView.h"
+
 @implementation FWScrollView
 
 - (instancetype)init
 {
     if (self = [super init]) {
+        self.topConstraint = nil;
+        self.leftConstraint = nil;
+        self.widthConstraint = nil;
+        self.heightConstraint = nil;
     }
     return self;
 }
@@ -13,18 +20,75 @@
 {
     if (self = [super initWithFrame:frame]) {
         self.frame = frame;
+        self.topConstraint = nil;
+        self.leftConstraint = nil;
+        self.widthConstraint = nil;
+        self.heightConstraint = nil;
     }
     return self;
 }
 
 - (void)layoutSubviews {
+    int height = 4000;
+    if ([[self subviews] count] >= 1) {
+        UIView * view = [[self subviews] lastObject];
+        // height = [self calcIntrinsicHeight:view];
+    }
+
+    self.heightConstraint.constant = height;
+
     [super layoutSubviews];
 
-    CGRect contentRect = CGRectZero;
-    for (UIView *view in self.subviews) {
-	contentRect = CGRectUnion(contentRect, view.frame);
+    self.contentSize = CGSizeMake(self.superview.frame.size.width, height);
+}
+
+- (int)calcIntrinsicHeight:(UIView *)view {
+    if ([view isKindOfClass:LinearLayoutView.class]) {
+        LinearLayoutView * child = (LinearLayoutView*)view;
+        int height = 0;
+        if (child.orientation == LinearLayoutViewOrientationVertical) {
+            for (LayoutParams *item in child.items) {
+                if (item.view.hidden) continue;
+                
+                height += item.margin.top + item.margin.bottom;
+                if (item.fixedHeight > 0) {
+                    height += item.fixedHeight;
+                } else {
+                    height += [self calcIntrinsicHeight:item.view] + item.padding.top + item.padding.bottom;
+                }
+            }
+        } else {
+            for (LayoutParams *item in child.items) {
+                if (item.view.hidden) continue;
+
+                int h = 0;
+                if (item.fixedHeight > 0) {
+                    h = item.fixedHeight + item.margin.top + item.margin.bottom;
+                } else {
+                    h = [self calcIntrinsicHeight:item.view] + item.padding.top + item.padding.bottom + item.margin.top + item.margin.bottom;
+                }
+                if (h > height) height = h;
+            }
+        }
+        return height;
+    } else if ([view isKindOfClass:FrameLayoutView.class]) {
+        FrameLayoutView * child = (FrameLayoutView*)view;
+        int height = 0;
+        for (LayoutParams *item in child.items) {
+            if (item.view.hidden) continue;
+
+            int h = 0;
+            if (item.fixedHeight > 0) {
+                h = item.fixedHeight + item.margin.top + item.margin.bottom;
+            } else {
+                h = [self calcIntrinsicHeight:item.view] + item.padding.top + item.padding.bottom + item.margin.top + item.margin.bottom;
+            }
+            if (h > height) height = h;
+        }
+        return height;
+    } else {
+        return view.intrinsicContentSize.height;
     }
-    self.contentSize = CGSizeMake(self.frame.size.width, contentRect.size.height);
 }
 
 @end
