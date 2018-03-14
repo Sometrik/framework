d106acff7caf9f6a1b412ed4550cc0fb382ed0ce
Mikael Rekola <mikael.rekola@gmail.com>
2018-03-14 20:49:30 +0200

    add picker

--- example1/ImageDialog.h
+++ example1/ImageDialog.h
@@ -7,6 +7,8 @@
 #include <Button.h>
 #include <ImageElement.h>
 #include <TextField.h>
+#include <Picker.h>
+#include <Logger.h>
 
 #include <memory>
 
@@ -28,13 +30,24 @@ public:
     auto textField = std::make_shared<TextField>();
     textField->style("width", "match-parent");
     layout->addChild(textField);
+
+    auto picker = std::make_shared<Picker>();
+    picker->addOption(0, "Option 1");
+    picker->addOption(2, "Option 2");
+    picker->addOption(3, "Option 3");
+    picker->style("width", "match-parent");
+    picker->style("color", "#000000");
+    layout->addChild(picker);
     
-    auto button = std::make_shared<Button>("OK");
+    auto button = std::make_shared<Button>("OK", 123);
     layout->addChild(button);
   }
   
   void onCommandEvent(CommandEvent & ev) override {
-    endModal(1);
+    if (ev.getElementId() == 123) {
+      getApplication().getLogger().println("Ending dialog");
+      endModal(1);
+    }
   }
 };
 
