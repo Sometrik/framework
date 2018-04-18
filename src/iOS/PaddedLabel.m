331f78ad24b74e4eca4b16744f0fcf8908d44367
Mikael Rekola <mikael.rekola@gmail.com>
2018-04-18 16:53:48 +0300

    remove inserts from adjustedBoundsWidth

--- src/iOS/PaddedLabel.m
+++ src/iOS/PaddedLabel.m
@@ -25,7 +25,7 @@
 }
 
 - (void)setBounds:(CGRect)bounds {
-  int adjustedBoundsWidth = bounds.size.width - (self.edgeInsets.left + self.edgeInsets.right + 2);
+  int adjustedBoundsWidth = bounds.size.width; // - (self.edgeInsets.left + self.edgeInsets.right + 2);
   if (adjustedBoundsWidth < self.preferredMaxLayoutWidth) {
       NSLog(@"Bounds changed %f => %f", self.bounds.size.width, bounds.size.width);
     [super setBounds:bounds];
