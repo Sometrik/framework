0ecf0041582297889960ad77bdcf1709989f9f1a
Mikael Rekola <mikael.rekola@gmail.com>
2018-04-19 13:09:52 +0300

    add method definition

--- src/iOS/PaddedLabel.h
+++ src/iOS/PaddedLabel.h
@@ -3,6 +3,7 @@
 @interface PaddedLabel : UILabel
 - (void)setBounds:(CGRect)bounds;
 - (CGSize)intrinsicContentSize;
+- (void)relayoutAll;
 @property (nonatomic, assign) UIEdgeInsets edgeInsets;
 
 @end
