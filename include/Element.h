bbb92a0775b3b6dd34b547282f8837af2d636538
Mikael Rekola <mikael.rekola@gmail.com>
2018-04-25 03:30:12 +0300

    add method focus()

--- include/Element.h
+++ include/Element.h
@@ -24,6 +24,7 @@ class Element : public EventHandler {
   
   virtual void show();
   virtual void hide();
+  virtual void focus();
   virtual void refresh() { }
   virtual void text(const std::string & text) { }
 
