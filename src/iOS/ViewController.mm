f925401aef53ee85e72085d9282cbe8462e99033
Mikael Rekola <mikael.rekola@gmail.com>
2018-05-12 00:19:34 +0300

    change image format

--- src/iOS/ViewController.mm
+++ src/iOS/ViewController.mm
@@ -2263,7 +2263,7 @@ static const CGFloat sideMenuOpenSpaceWidth = 100.0;
     if (url != nil) {
       s = [url cStringUsingEncoding:NSUTF8StringEncoding];
     }
-    mainThread->sendImageRequest((int)imageView.tag, size.width, 0, s, 5); // RGBA8
+    mainThread->sendImageRequest((int)imageView.tag, size.width, 0, s, 15); // RGB555
 }
 
 - (void)didCancelImageRequest:(FWImageView *)imageView
