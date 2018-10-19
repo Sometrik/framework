5c83a3e29996fb18cf0329eec698e1ed93914485
Mikael Rekola <mikael.rekola@gmail.com>
2018-10-19 22:54:35 +0300

    fix typo

--- src/iOS/ViewStyle.h
+++ src/iOS/ViewStyle.h
@@ -3,7 +3,7 @@
 @interface ViewStyle : NSObject
 - (void)apply:(UIView *)view animate:(BOOL)animate;
 
-@property (nonatomic, string) NSString * fontFamily;
+@property (nonatomic, strong) NSString * fontFamily;
 @property (nonatomic, assign) NSInteger fontSize;
 @property (nonatomic, assign) NSInteger fontWeight;
 @property (nonatomic, assign) float alpha;
