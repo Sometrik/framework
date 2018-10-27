9646ec145ca4d75678c09467f4d75ec1c154f33b
Mikael Rekola <mikael.rekola@gmail.com>
2018-10-28 01:58:51 +0300

    add class AppRunner

---
+++ include/AppRunner.h
@@ -0,0 +1,26 @@
+#ifndef _APPRUNNER_H_
+#define _APPRUNNER_H_
+
+#include <Runnable.h>
+
+class AppRunner : public Runnable {
+ public:
+  AppRunner() { }
+  
+  void run() override {
+    auto thread = getThreadPtr();
+    if (thread.get()) {
+      auto & app = thread->getApplication();
+      app.initialize(thread);
+
+      thread->startEventLoop();
+#if 0
+      deinitializeRenderer();
+#endif
+      Command c(Command::QUIT_APP, getInternalId());
+      thread->sendCommand(c);
+    }
+  }  
+};
+
+#endif
