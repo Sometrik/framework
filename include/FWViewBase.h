2a50b67cdcfa0770ac8566e6384eb89f43ebf943
Mikael Rekola <mikael.rekola@gmail.com>
2018-11-26 10:05:30 +0200

    remove title

--- include/FWViewBase.h
+++ include/FWViewBase.h
@@ -8,8 +8,6 @@
 class FWViewBase : public Element {
  public:
   FWViewBase(int _id) : Element(_id) { }
-  FWViewBase(const std::string & _title, int _id)
-    : Element(_id), title(_title) { }
 
   bool isA(const std::string & className) const override {
     if (className == "FWViewBase") return true;
@@ -27,20 +25,16 @@ class FWViewBase : public Element {
     
     Command c1(Command::SET_INT_VALUE, getInternalId());
     c1.setValue(1);
-    c1.setTextValue(getTitle());
     sendCommand(c1);
 
     Command c2(Command::SET_INT_VALUE, getApplication().getInternalId(), getInternalId());
     c2.setValue(1);
-    c2.setTextValue(getTitle());
     sendCommand(c2);
   }
 
   bool isVisible() const override {
     return getApplication().getActiveViewId() == getInternalId();
   }
-
-  virtual std::string getTitle() const { return title; }
 
  protected:
   void prepare() override {
@@ -49,9 +43,6 @@ class FWViewBase : public Element {
       app.setActiveViewId(getInternalId());
     }
   }
-  
- private:
-  std::string title;
 };
 
 #endif
